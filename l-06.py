c67356ad8a30a398552977b1de02834351581186
aleksmn <al-mn@yandex.ru>
2022-05-07 09:49:52 +0300

    add palindrom

--- l-06.py
+++ l-06.py
@@ -4,19 +4,53 @@
 # иначе — 0.
 
 
-pin = '1234'
+# pin = '1234'
 
-if pin.isnumeric() and len(pin) == 4:
-    print(1)
+# if pin.isnumeric() and len(pin) == 4:
+#     print(1)
 
 
 # Дана строка a. Если данная строка является палиндромом, то выведи 1, иначе — 0.
 
 
-a = 'доход1'
+# Cпособ 1
+# string = "доход"
+
+# reversed = ""
 
 
-if(a[::-1] == a):
+# for i in range(1, len(string)+1):
+#     reversed += string[-i]
+
+# if (reversed == string):
+#     print(1)
+# else:
+#     print(0)
+
+
+# Cпособ 2
+
+string = "доход"
+
+reversed = ""
+
+
+for i in string:
+    reversed = i + reversed
+
+
+if (reversed == string):
     print(1)
 else:
     print(0)
+
+
+# Cпособ 3
+
+# a = 'доход'
+
+
+# if(a[::-1] == a):
+#     print(1)
+# else:
+#     print(0)
