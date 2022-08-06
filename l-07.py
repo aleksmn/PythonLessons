65ed808f0450433b3e04f857bb1670876a842a2f
aleksmn <al-mn@yandex.ru>
2022-08-06 18:05:20 +0300

    init

---
+++ l-07.py
@@ -0,0 +1,27 @@
+# Даны 3 целых числа a, b и c. Выведи количество одинаковых чисел.
+
+
+# a, b, c = 7, 0, 10
+
+# if a == b and a == c and c == b:
+#     print(3)
+# elif a != b and a != c and c != b:
+#     print(1)
+# else:
+#     print(2)
+
+
+# Даны 3 числа a, b и c. Необходимо найти наибольшее из них.
+
+# a, b, c = 7, 1, 2
+
+# if a > b and a > c:
+#     maximum = a
+# elif b > a and b > c:
+#     maximum = b
+# elif c > a and c > b:
+#     maximum = c
+# else:
+#     maximum = None
+
+# print(maximum)
