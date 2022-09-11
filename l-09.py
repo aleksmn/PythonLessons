f502c966dae4da9eb40ca557db2365834e90c5a9
aleksmn <al-mn@yandex.ru>
2022-09-11 16:03:28 +0300

    add tasks

--- l-09.py
+++ l-09.py
@@ -1,22 +1,29 @@
+# s = "81005557889"
+
+# formated = f'+7 ({s[1:4]}) {s[4:7]}-{s[7:9]}-{s[9:]}'
+
+# print(formated)
+
+
 # Калькулятор
 
-num1 = float(input("Первое число: "))
-num2 = float(input("Второе число: "))
+# num1 = float(input("Первое число: "))
+# num2 = float(input("Второе число: "))
 
-# выбираем оператор
-operation = input("Введите действие (+, -, *, /): ")
+# # выбираем оператор
+# operation = input("Введите действие (+, -, *, /): ")
 
-if operation == "+":
-    print(num1, "+", num2, "=", num1 + num2)
+# if operation == "+":
+#     print(num1, "+", num2, "=", num1 + num2)
 
-elif operation == "-":
-    print(num1, "-", num2, "=", num1 - num2)
+# elif operation == "-":
+#     print(num1, "-", num2, "=", num1 - num2)
 
-elif operation == "*":
-    print(num1, "*", num2, "=", num1 * num2)
+# elif operation == "*":
+#     print(num1, "*", num2, "=", num1 * num2)
 
-elif operation == "/":
-    print(num1, "/", num2, "=", num1 / num2)
+# elif operation == "/":
+#     print(num1, "/", num2, "=", num1 / num2)
 
-else:
-    print("Неправильный ввод")
+# else:
+#     print("Неправильный ввод")
