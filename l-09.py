faedc901a4538d8df29072246038b0739cf1f5df
aleksmn <al-mn@yandex.ru>
2022-09-17 17:11:17 +0300

    add try except

--- l-09.py
+++ l-09.py
@@ -7,23 +7,32 @@
 
 # Калькулятор
 
-# num1 = float(input("Первое число: "))
-# num2 = float(input("Второе число: "))
+num1 = input("Первое число: ")
+num2 = input("Второе число: ")
 
-# # выбираем оператор
-# operation = input("Введите действие (+, -, *, /): ")
 
-# if operation == "+":
-#     print(num1, "+", num2, "=", num1 + num2)
+try:
+    num1 = float(num1)
+    num2 = float(num2)
+except:
+    print("Неверный ввод")
+    quit()
 
-# elif operation == "-":
-#     print(num1, "-", num2, "=", num1 - num2)
 
-# elif operation == "*":
-#     print(num1, "*", num2, "=", num1 * num2)
+# выбираем оператор
+operation = input("Введите действие (+, -, *, /): ")
 
-# elif operation == "/":
-#     print(num1, "/", num2, "=", num1 / num2)
+if operation == "+":
+    print(num1, "+", num2, "=", num1 + num2)
 
-# else:
-#     print("Неправильный ввод")
+elif operation == "-":
+    print(num1, "-", num2, "=", num1 - num2)
+
+elif operation == "*":
+    print(num1, "*", num2, "=", num1 * num2)
+
+elif operation == "/":
+    print(num1, "/", num2, "=", num1 / num2)
+
+else:
+    print("Неправильный ввод")
