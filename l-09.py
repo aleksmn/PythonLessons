719b8180573872b142309044369487e78ce968fd
aleksmn <al-mn@yandex.ru>
2022-09-17 17:29:16 +0300

    fix

--- l-09.py
+++ l-09.py
@@ -1,38 +1,44 @@
-# s = "81005557889"
+s = "81005557889"
 
-# formated = f'+7 ({s[1:4]}) {s[4:7]}-{s[7:9]}-{s[9:]}'
+# +7 (100) 555-78-89
 
-# print(formated)
+formated = f'+7 ({s[1:4]}) {s[4:7]}-{s[7:9]}-{s[9:]}'
+
+
+print(formated)
 
 
 # Калькулятор
 
-num1 = input("Первое число: ")
-num2 = input("Второе число: ")
+# num1 = input("Первое число: ")
+# num2 = input("Второе число: ")
 
 
-try:
-    num1 = float(num1)
-    num2 = float(num2)
-except:
-    print("Неверный ввод")
-    quit()
+# try:
+#     num1 = float(num1)
+#     num2 = float(num2)
+# except:
+#     print("Неверный ввод")
+#     quit()
 
 
-# выбираем оператор
-operation = input("Введите действие (+, -, *, /): ")
+# # выбираем оператор
+# operation = input("Введите действие (+, -, *, /): ")
 
-if operation == "+":
-    print(num1, "+", num2, "=", num1 + num2)
+# if operation == "+":
+#     print(num1, "+", num2, "=", num1 + num2)
 
-elif operation == "-":
-    print(num1, "-", num2, "=", num1 - num2)
+# elif operation == "-":
+#     print(num1, "-", num2, "=", num1 - num2)
 
-elif operation == "*":
-    print(num1, "*", num2, "=", num1 * num2)
+# elif operation == "*":
+#     print(num1, "*", num2, "=", num1 * num2)
 
-elif operation == "/":
-    print(num1, "/", num2, "=", num1 / num2)
+# elif operation == "/":
+#     print(num1, "/", num2, "=", num1 / num2)
 
-else:
-    print("Неправильный ввод")
+# else:
+#     print("Неправильный ввод")
+
+
+# input()
