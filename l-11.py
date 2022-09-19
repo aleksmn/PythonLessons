8d936ae1f4ac7d1d0f69cfdb7e8ed17326d91863
aleksmn <al-mn@yandex.ru>
2022-09-19 16:54:04 +0300

    fix

--- l-11.py
+++ l-11.py
@@ -9,17 +9,17 @@
 # print(f"Факториал числа {n}: {fact}")
 
 
-fact_sum = 0
+# fact_sum = 0
 
-for n in range(1, 8):
+# for n in range(1, 8):
 
-    fact = 1
+#     fact = 1
 
-    for i in range(1, n+1):
-        fact = fact * i
+#     for i in range(1, n+1):
+#         fact = fact * i
 
-    fact_sum += fact
-    print(f"Факториал числа {n}: {fact}.  Сумма факториалов: {fact_sum}")
+#     fact_sum += fact
+#     print(f"Факториал числа {n}: {fact}.  Сумма факториалов: {fact_sum}")
     
     
 
