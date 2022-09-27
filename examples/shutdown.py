e0b54b4c5555b5dd4d1d53cc1998d5139abb86f2
aleksmn <al-mn@yandex.ru>
2022-09-27 20:19:54 +0300

    init

--- examples/shutdown.py
+++ examples/shutdown.py
@@ -2,4 +2,9 @@ import os
 
 # os.system("shutdown /s /t 30")
 
-os.system("shutdown 30")
+minutes = input('Через сколько минут выключить компьютер?\n')
+
+# seconds = int(minutes) * 60 
+
+
+os.system("shutdown " + str(minutes))
