6ccfee8a61d886213e9d5bc626ebb32c764f40fd
aleksmn <al-mn@yandex.ru>
2022-10-02 15:20:57 +0300

    add task

--- l-15.py
+++ l-15.py
@@ -1,16 +1,38 @@
+
+#Список товаров
+
+n = int(input("Сколько товаров? "))
+
+arr = []
+
+for x in range(n):
+    arr.append([input("Товар: "), input("Вес: ")])
+
+
+print(arr)
+
+for t in arr:
+    print(t)
+
+
+
+
+
+
+
 # Дано целое положительное число A. 
 # Следует создать и вывести список размера A, 
 # содержащий A первых положительных нечётных чисел.
 
 
-print("Находим N первых положительных чисел")
-n = int(input("Введите число: "))
+# print("Находим N первых положительных чисел")
+# n = int(input("Введите число: "))
 
-arr = []
-i = 1
+# arr = []
+# i = 1
 
-for x in range(n):
-    arr.append(i)
-    i = i + 2
+# for x in range(n):
+#     arr.append(i)
+#     i = i + 2
 
-print(arr)
+# print(arr)
