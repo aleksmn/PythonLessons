5a887ce5110577d2af1f3857822d6fc4ae836c52
aleksmn <al-mn@yandex.ru>
2022-10-10 16:59:30 +0300

    init

---
+++ l-19.py
@@ -0,0 +1,20 @@
+# def getSum(arr):
+#     sum = 0
+#     for x in arr:
+#         try:
+#             x = int(x)
+#         except:
+#             x = 1
+#         sum = sum + x
+#     return sum
+
+
+# def minNumber(arr):
+#     smallest = None
+#     arr = [int(x) for x in arr]
+#     print(arr)
+#     for i in arr:
+#         if smallest is None or i < smallest:
+#             smallest = i
+#         #print(i, smallest)
+#     print("Минимум:", smallest)
