3f0622602a2b94725d364c4aa6dc418ced22dfca
aleksmn <al-mn@yandex.ru>
2022-10-19 11:36:42 +0300

    init

---
+++ examples/sets.py
@@ -0,0 +1,22 @@
+
+a = {1, 2, 3}
+
+b = {1, 2, 3, 4, 5}
+
+
+# print(a <= b)
+
+c = a.union(b)
+
+a.add(99)
+
+x = a.difference(c)
+
+print('a', a)
+print('b', b)
+print('c', c)
+print('x', x)
+
+
+
+
