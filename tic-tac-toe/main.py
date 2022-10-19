754b3371b6121f98f71afb08b17b4d1c025f40fd
aleksmn <al-mn@yandex.ru>
2022-10-19 13:02:49 +0300

    init

---
+++ tic-tac-toe/main.py
@@ -0,0 +1,79 @@
+board = list(range(1, 10))
+
+# print(board)
+
+# Параметры поля
+cells = 3
+dashes = 13
+spaces = 4
+
+# Счетчик ходов
+counter = 0
+
+is_win = False
+
+# Условия попеды
+win_coords = (
+    (0, 1, 2), (3, 4, 5), (6, 7, 8),
+    (0, 3, 6), (1, 4, 7), (2, 5, 8),
+    (0, 4, 8), (2, 4, 6)
+)
+
+
+def draw_board():
+    '''Отрисовка поля в консоли'''
+
+    for i in range(3):
+        shift = i * 3
+        print(spaces * ' ', end="")
+        print('-' * dashes)
+        print(spaces * ' ', end="")
+        print(
+            f'| {board[0 + shift]} | {board[1 + shift]} | {board[2 + shift]} |')
+
+    print(spaces * ' ', end="")
+    print('-' * dashes + "\n")
+
+
+print("\n* Игра Крестики-нолики *\n")
+
+
+# Игровой цикл
+
+while not is_win:
+
+    draw_board()
+
+    if counter % 2 == 0:
+        player_token = 'X'
+    else:
+        player_token = 'O'
+
+    player_answer = input(f'Куда ставим {player_token}?: ')
+
+    player_answer = int(player_answer)
+
+    if str(board[player_answer - 1]) not in 'XO':
+        board[player_answer - 1] = player_token
+    else:
+        print('Эта ячейка уже занята!')
+        continue
+
+    counter += 1
+
+    if counter > 4:
+        for each in win_coords:
+            # Проверяем наличие выигрышной комбинации (для X или O)
+            if board[each[0]] == board[each[1]] == board[each[2]]:
+                is_win = True
+                break
+
+        if is_win:
+            draw_board()
+            print(f'Победил {player_token}')
+            break
+
+    if counter == 9:
+        draw_board()
+        print('Победила дружба :)')
+        break
