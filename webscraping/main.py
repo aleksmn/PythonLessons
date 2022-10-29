f060e7a91c540e7a32fa4cb410771aef764c9f66
aleksmn <al-mn@yandex.ru>
2022-10-29 13:39:05 +0300

    add finding

--- webscraping/main.py
+++ webscraping/main.py
@@ -1,3 +1,26 @@
 from bs4 import BeautifulSoup
 
+with open("index.html", "r") as f:
+    doc = BeautifulSoup(f, "html.parser")
+
+
+# print(doc.prettify())
+
+# Выбираем один элемент по тегу (только первый)
+tag = doc.h1
+print(tag.string)
+
+
+
+# Выбираем несколько элементов по тегу
+tags = doc.find_all("a")
+# print(tags)
+
+links = []
+
+for t in tags:
+    print(t)
+    # Выбираем ссылки
+    # print(t.attrs['href'])
+    # links.append(t.attrs['href'])
 
