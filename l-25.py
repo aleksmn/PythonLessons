f7f59a4fb722bae0f249aa5906544fe091a82a17
aleksmn <al-mn@yandex.ru>
2022-10-31 17:55:47 +0300

    init

--- l-25.py
+++ l-25.py
@@ -1,3 +1,67 @@
+# Список:
+# users = ['Дима', 'Юра', 'Ваня']
+
+
+# Словарь
+
+# 'ключ':'значение',
+
+# user = {'name': 'Михаил', 'tel': '81235467654', 'status': 'учитель'}
+
+
+rus_eng = {'one': 'один'}
+
+rus_eng['четыре'] = 'four'
+
+print(rus_eng)
+
+
+while True:
+    key = input("Введите слово на русском\n")
+    value = input("Введите перевод на английском\n")
+    if(key=='' or  value ==''):
+        break
+    rus_eng[key] = value
+    print(rus_eng)
+
+
+print(rus_eng)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # Цепочка ДНК состоит из двух рядов нуклеотидов, соединённых вместе. 
 # Интересно, что нуклеотид A может соединяться только с нуклеотидом T, C с G 
 # и наоборот. Получается, зная содержимое одной стороны цепочки ДНК, 
@@ -7,19 +71,19 @@
 # и возвращает её пару.
 
 
-def DNA_reverse(dna: str):
-    new_dna = ""
-    pairs = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+# def DNA_reverse(dna: str):
+#     new_dna = ""
+#     pairs = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
 
-    for key in dna:
-        new_dna += pairs[key]
+#     for key in dna:
+#         new_dna += pairs[key]
     
-    return new_dna
+#     return new_dna
 
 
-test1 = "ATCG"
-test2 = "ACTAGCGTCAGCTAGCTCGGTCAATTCGCTATGCGATCGCGCTTTAAC"
+# test1 = "ATCG"
+# test2 = "ACTAGCGTCAGCTAGCTCGGTCAATTCGCTATGCGATCGCGCTTTAAC"
 
-print(DNA_reverse(test1))
+# print(DNA_reverse(test1))
 
-print(DNA_reverse(test2))
+# print(DNA_reverse(test2))
