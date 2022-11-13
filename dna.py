43587839fa9917fa5c622b50cf1f065f6d35b495
aleksmn <al-mn@yandex.ru>
2022-11-13 14:56:36 +0300

    fix

--- dna.py
+++ dna.py
@@ -1,10 +1,25 @@
-# Напиши функцию, которая будет принимать аргументом цепочку ДНК 
-# и проверять её правильность. Если в цепочке встречаются какие-то 
-# символы помимо A, T, C и G, функция должна возвращать False, 
-# а если цепочка правильная, то True. Функция должна быть чистой 
+def DNA_reverse(dna: str):
+    new_dna = ""
+    pairs = {'A': 'T', 'T': 'A', 'C': 'G', 'G': 'C'}
+
+    for key in dna:
+        new_dna += pairs[key]
+
+    return new_dna
+
+
+test1 = "ATCG"
+test2 = "ACTAGCGTCAGCTAGCTCGGTCAATTCGCTATGCGATCGCGCTTTAAC"
+
+print(DNA_reverse(test1))
+print(DNA_reverse(test2))
+
+
+# Напиши функцию, которая будет принимать аргументом цепочку ДНК
+# и проверять её правильность. Если в цепочке встречаются какие-то
+# символы помимо A, T, C и G, функция должна возвращать False,
+# а если цепочка правильная, то True. Функция должна быть чистой
 # и использовать подсказки типов.
-
-
 
 
 dna = "ATCG"
@@ -15,9 +30,8 @@ def check_dna(dna_for_test: str):
     for s in input_dna.upper():
         if s in dna:
             return False
-        
+
     return True
 
 
 print(check_dna(input_dna))
-
