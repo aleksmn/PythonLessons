1cc10ee4c1918c0e4fe82f680108f4de4226c36f
aleksmn <al-mn@yandex.ru>
2022-11-15 19:59:08 +0300

    init

---
+++ l-27.py
@@ -0,0 +1,30 @@
+# Объек
+class Cat:
+    
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        print(self.name + ' говорит мяу!')
+
+
+
+murka = Cat("Мурка")
+
+murka.speak()
+
+print(dir(murka))
+
+
+
+
+
+
+
+
+
+
+
+murz = Cat('Мурзик')
+
+murz.speak()
