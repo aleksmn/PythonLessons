84b98c92da7b0439c414323a7b52cde6b8b696f4
aleksmn <al-mn@yandex.ru>
2022-12-02 17:01:51 +0300

    fix

--- l-14.py
+++ l-14.py
@@ -1,45 +1,61 @@
-letters = ['п', 'р', 'и', 'в', 'е', 'т']
+# letters = ['п', 'р', 'и', 'в', 'е', 'т']
 
-mystring = "*".join(letters)
+# mystring = "*".join(letters)
 
-print(mystring)
-
-
-print(list(mystring))
+# print(mystring)
 
 
+# print(list(mystring))
+
+
+# Трудный Тест
+
+# a = 0
+
+# for temp in range(5):
+
+#     a += 1
+
+#     if a == 3:
+#         a += 2
+
+#     if a == 4:
+#         a += 3
+
+#     if a == 5:
+#         a += 4
+
+#     else:
+#         print('зашли в else')
+#         a += 1
+
+#     print('Шаг', temp)
+#     print('a = ', a)
+
+# print(a)
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-# Дано целое положительное число N. 
-# Выведи все числа Фибоначчи до числа, большего N. 
+# Дано целое положительное число N.
+# Выведи все числа Фибоначчи до числа, большего N.
 # Вывод чисел запиши через пробел.
 
 
 # n = 13
 
-# # 1 1 2 3 5
+# # # 0 1 1 2 3 5
 
-# a = 1
-# b = 1
 
-# print(1)
+# f1 = 1
+# f2 = 1
 
-# while a <= n:
-#     temp = a
-#     a = b
-#     b = temp + b
-#     print(a)
+# print(1, end=' ')
+
+# while f1 <= n:
+#     temp = f1
+#     f1 = f2
+#     f2 = temp + f1
+#     print(f1, end=' ')
+
+# print()
