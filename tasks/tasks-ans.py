b9747d1333bd674fe57ce4b158f2ae4c876ed14f
aleksmn <al-mn@yandex.ru>
2022-12-12 17:53:14 +0300

    init

--- tasks/tasks-ans.py
+++ tasks/tasks-ans.py
@@ -5,19 +5,57 @@
 
 
 
-word = input('Введите слово\n')
+# word = input('Введите слово\n')
 
-g = 'аоуыиэеёюя'
-s = 'бвгджзйклмнпрстфхцчшщ'
+# g = 'аоуыиэеёюя'
+# s = 'бвгджзйклмнпрстфхцчшщ'
 
-g_count = 0
-s_count = 0
+# g_count = 0
+# s_count = 0
 
-for letter in word.lower():
-    if letter in g:
-        g_count += 1
+# for letter in word.lower():
+#     if letter in g:
+#         g_count += 1
 
 
-print('Гласные:', g_count)
+# print('Гласные:', g_count)
 
-print('Согласные:', s_count)
+# print('Согласные:', s_count)
+
+
+
+
+
+
+
+
+
+# Задача 2
+# Какого цвета клетка?
+# Пользователь вводи координату клетки шахматной доски, например a5
+# Необходимо определить, какого цвета эта клетка.
+
+
+coords_input = input('Write coords, for example a5: \n')
+
+letters = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
+
+
+try:
+
+    x = letters[coords_input[0]]
+    y = int(coords_input[1])
+
+except:
+    print('Неверный ввод')
+    quit()
+
+
+
+print(x, y)
+
+if (x + y) % 2 == 0:
+    print('Черная клетка')
+else:
+    print('Белая клетка')
+
