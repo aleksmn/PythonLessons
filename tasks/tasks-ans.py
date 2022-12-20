041ca530465131049102ec337cebfd0970019fbe
aleksmn <al-mn@yandex.ru>
2022-12-20 19:30:05 +0300

    fix

--- tasks/tasks-ans.py
+++ tasks/tasks-ans.py
@@ -36,26 +36,59 @@
 # Необходимо определить, какого цвета эта клетка.
 
 
-coords_input = input('Write coords, for example a5: \n')
+# coords_input = input('Write coords, for example a5: \n')
 
-letters = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
-
-
-try:
-
-    x = letters[coords_input[0]]
-    y = int(coords_input[1])
-
-except:
-    print('Неверный ввод')
-    quit()
+# letters = {'a': 1, 'b': 2, 'c': 3, 'd': 4, 'e': 5, 'f': 6, 'g': 7, 'h': 8}
 
 
+# try:
 
-print(x, y)
+#     x = letters[coords_input[0]]
+#     y = int(coords_input[1])
 
-if (x + y) % 2 == 0:
-    print('Черная клетка')
+# except:
+#     print('Неверный ввод')
+#     quit()
+
+
+
+# print(x, y)
+
+# if (x + y) % 2 == 0:
+#     print('Черная клетка')
+# else:
+#     print('Белая клетка')
+
+
+# Задача 4
+# Написать программу для подсчета
+# среднего значения всех введенных пользователем чисел. 
+# Индикатором окончания ввода будет служить ввод пустой строки.
+
+
+nums = []
+
+while True:
+
+    user_input = input('Введите число: ')
+
+    if user_input == '':
+        break
+
+    try:
+        user_input = float(user_input)
+        nums.append(user_input)
+    except:
+        pass
+
+    
+print('--------------------')
+
+if len(nums) > 0:
+
+    print(nums)
+    print('Среднее значение: ')
+    print(sum(nums) / len(nums))
+
 else:
-    print('Белая клетка')
-
+    print('Неверный ввод')
