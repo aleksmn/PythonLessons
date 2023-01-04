84c44167c6287bb7e306dfc2d91c1c70c01f18c9
aleksmn <al-mn@yandex.ru>
2023-01-04 18:42:49 +0300

    init

---
+++ module-1/turtle-demo/abstract.py
@@ -0,0 +1,18 @@
+import turtle
+from random import randint
+
+turtle.screensize(500, 500)
+turtle.shape("turtle")
+turtle.pencolor("blue")
+turtle.fillcolor("blue")
+
+for i in range(10):
+    turtle.goto(randint(-250, 250), randint(-250, 250))
+    w = randint(10, 100)
+    turtle.begin_fill()
+    for j in range(4):
+        turtle.forward(w)
+        turtle.right(90)
+    turtle.end_fill()
+
+turtle.mainloop()
