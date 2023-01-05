96df3feeb6b66710ea0af30cd803ba5375ad812a
aleksmn <al-mn@yandex.ru>
2023-01-05 19:22:41 +0300

    init

---
+++ module-2/sets_demo.py
@@ -0,0 +1,44 @@
+# Множества 
+# Set
+
+prog = {'CSS', 'Python', 'HTML', 'C++', 'Go'}
+
+lang = {'Python', 'Java','Go', 'CSS', 'Rust'}
+
+
+
+# prog.add('C#')
+# print(prog)
+
+# lang.remove('Go')
+# print(lang)
+
+# Union (объединение)
+# my_stack_1 = prog.union(lang)
+# my_stack_2 = prog | lang
+
+# print('Union:', my_stack_1)
+# print('Union:', my_stack_2)
+
+# Intersection (пересечение)
+# my_stack_1 = prog.intersection(lang)
+# my_stack_2 = prog & lang
+
+# print('Intersection:', my_stack_1)
+# print('Intersection:', my_stack_2)
+
+
+# Difference (разность)
+# my_stack_1 = prog.difference(lang)
+# my_stack_2 = prog - lang
+
+# print('Difference:', my_stack_1)
+# print('Difference:', my_stack_2)
+
+
+# Symmetric difference (Симметричная разность)
+# my_stack_1 = prog.symmetric_difference(lang)
+# my_stack_2 = prog ^ lang
+
+# print('Symmetric difference:', my_stack_1)
+# print('Symmetric difference:', my_stack_2)
