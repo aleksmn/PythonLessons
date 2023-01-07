e4100cfaa443e177206f40361419c154a4b70b2a
aleksmn <al-mn@yandex.ru>
2023-01-07 18:52:13 +0300

    init

---
+++ module-1/data_types.py
@@ -0,0 +1,24 @@
+# print('Целое число: ')
+# x = 42
+# print(type(x))
+# print(x * 3)
+
+
+# print('Дробное число: ')
+# b = 2.5
+# print(type(b))
+# print(b * 3)
+
+
+# print('Строка (str):')
+
+# s = 'Привет, как дела?'
+
+# print(s[1])
+# print(s[-1])
+
+# # Срез
+# print(s[2:])
+
+
+# print('в' in s)
