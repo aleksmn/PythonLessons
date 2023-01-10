58275be98398bed6e35d7e1c8e07c9999e271cd8
aleksmn <al-mn@yandex.ru>
2023-01-10 19:24:50 +0300

    init

---
+++ module-1/turtle-demo/fence.py
@@ -0,0 +1,15 @@
+import turtle
+
+turtle.speed(10)
+for i in range(0, 5):
+    turtle.forward(10)
+    turtle.left(90)
+    turtle.forward(30)
+    turtle.right(90)
+    turtle.forward(10)
+    turtle.right(90)
+    turtle.forward(30)
+    turtle.left(90)
+
+
+turtle.mainloop()
