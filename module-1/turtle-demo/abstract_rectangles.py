b493c7e6e1aaee1c1e618fa6642013ccc70ac3f6
aleksmn <al-mn@yandex.ru>
2023-01-12 15:53:47 +0300

    fix

--- module-1/turtle-demo/abstract_rectangles.py
+++ module-1/turtle-demo/abstract_rectangles.py
@@ -2,17 +2,17 @@ import turtle
 import random
 
 
-def draw_retangle(x, y, w, h, color):
-    turtle.up()
-    turtle.seth(0)
+def draw_rectangle(x, y, w, h, color):
+    turtle.penup()
+    turtle.setheading(0)
     turtle.goto(x-w/2, y-h/2)
-    turtle.fillcolor(color)
-    turtle.down()
+    turtle.color(color)
+    turtle.pendown()
     turtle.begin_fill()
     for i in range(2):
-        turtle.fd(w)
+        turtle.forward(w)
         turtle.left(90)
-        turtle.fd(h)
+        turtle.forward(h)
         turtle.left(90)
     turtle.end_fill()
 
@@ -23,7 +23,7 @@ turtle.speed(0)
 turtle.hideturtle()
 n = 100
 for i in range(n):
-    draw_retangle(random.randint(-300, 300), random.randint(-300, 300),
+    draw_rectangle(random.randint(-300, 300), random.randint(-300, 300),
                   random.randint(5, 100), random.randint(5, 100),
                   (random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)))
 
--- module-1/turtle-demo/rectangle.py
+++ module-1/turtle-demo/rectangle.py
@@ -2,21 +2,31 @@ import turtle
 import random
 
 
-def draw_retangle(x, y, w, h, color):
-    turtle.up()
-    turtle.seth(0)
+def draw_rectangle(x, y, w, h, color):
+    turtle.penup()
+    turtle.setheading(0)
     turtle.goto(x-w/2, y-h/2)
     turtle.fillcolor(color)
-    turtle.down()
+    turtle.pendown()
     turtle.begin_fill()
     for i in range(2):
-        turtle.fd(w)
+        turtle.forward(w)
         turtle.left(90)
-        turtle.fd(h)
+        turtle.forward(h)
         turtle.left(90)
     turtle.end_fill()
 
-draw_retangle(0, 0, 200, 100, 'red')
+
+
+
+
+rand_color = (random.random(), random.random(), random.random())
+
+print(rand_color)
+
+
+draw_rectangle(0, 0, 200, 100, rand_color)
+
 
 
 turtle.mainloop()
