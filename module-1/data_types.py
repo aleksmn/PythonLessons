72b60206d12147fdc36c354dcb036030ac01ade3
aleksmn <al-mn@yandex.ru>
2023-01-13 12:31:10 +0300

    init

--- module-1/data_types.py
+++ module-1/data_types.py
@@ -1,24 +1,88 @@
-# print('Целое число: ')
+# print('Целое число (integer, int): ')
 # x = 42
 # print(type(x))
-# print(x * 3)
+# print(x % 3)
 
 
-# print('Дробное число: ')
+# print('Дробное число (float): ')
 # b = 2.5
 # print(type(b))
 # print(b * 3)
 
 
+# n = float(input('Введите дробное число: '))
+# print(n**2)
+
+
 # print('Строка (str):')
 
-# s = 'Привет, как дела?'
+# # Индексы
+# #    0              -1
+# s = 'Я изучаю Python!'
 
-# print(s[1])
-# print(s[-1])
+# # Палиндром или нет?
+# p = 'Доход'
 
-# # Срез
-# print(s[2:])
-
+# print(p == p[::-1])
 
 # print('в' in s)
+
+
+# print("Булево значение (bool)")
+# # True
+# # False
+
+# b = True
+
+# print(type(b))
+
+# x = 2
+# print(100 > x)
+# print(20 == x)
+
+
+# a = ''
+# print(bool(a))
+
+
+# print("Тип данных None")
+
+# y = None
+
+# print(type(y))
+
+
+# print("Список (list):")
+
+# my_list = [234, 0, 'Привет', True, False, None]
+
+# print(my_list)
+
+# my_list[0] = 'До свидания!'
+
+# print(my_list)
+
+
+# print("Кортеж (tuple):")
+
+# my_tuple = (234, 0, 'Привет', True, False, None)
+
+# print(my_tuple)
+
+# my_tuple[0] = 'Пока!'    #TypeError
+
+
+
+
+# print("Словарь (dict): ")
+
+# my_dictionary = {
+#     'один': 'one',
+#     'два': 'two',
+#     'три': 'three'
+# }
+
+# print(my_dictionary)
+# print(type(my_dictionary))
+
+# print(my_dictionary['один'])
