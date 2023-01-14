b41e11b3afdcd829ae47b4ced8af536676d43ece
aleksmn <al-mn@yandex.ru>
2023-01-14 17:43:11 +0300

    init

---
+++ module-1/turtle-demo/abstract_to_file.py
@@ -0,0 +1,40 @@
+import turtle
+import random
+
+
+from svg_turtle import SvgTurtle
+
+turtle = SvgTurtle(700, 700)
+
+
+def draw_rectangle(x, y, w, h, color):
+    turtle.penup()
+    turtle.setheading(0)
+    turtle.goto(x-w/2, y-h/2)
+    
+    turtle.fillcolor(color)
+    turtle.pendown()
+    turtle.begin_fill()
+    for i in range(2):
+        turtle.forward(w)
+        turtle.left(90)
+        turtle.forward(h)
+        turtle.left(90)
+    turtle.end_fill()
+
+
+# turtle.setup(700, 700)
+# turtle.title("Random Rectangles")
+turtle.speed(0)
+turtle.hideturtle()
+
+n = 100
+for i in range(n):
+    draw_rectangle(random.randint(-300, 300), random.randint(-300, 300),
+                  random.randint(5, 100), random.randint(5, 100),
+                  (random.uniform(0, 1), random.uniform(0, 1), random.uniform(0, 1)))
+
+
+turtle.save_as('example.svg')
+
+# turtle.mainloop()
