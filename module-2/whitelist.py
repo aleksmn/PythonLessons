a17e2da207b8017f5be5757426d9d333736ef503
aleksmn <al-mn@yandex.ru>
2023-01-27 16:24:21 +0300

    init

--- module-2/whitelist.py
+++ module-2/whitelist.py
@@ -5,15 +5,18 @@ white_request = 1
 request = 1
 
 while white_request:
-  white_request = input('Разрешенный запрос:')
-  if white_request:
-    white_list.add(white_request)
+    white_request = input('Разрешенный запрос:')
+    if white_request:
+        white_list.add(white_request)
 
 while request:
-  request = input('Запрос: ')
-  if request:
-    answers.add(request)
+    request = input('Запрос: ')
+    if request:
+        answers.add(request)
 
-for answer in answers:
-  if answer in white_list:
-      print(answer)
+
+# Используем пересечения
+# print(white_list & answers)
+
+for a in white_list & answers:
+    print(a)
