c8885f9f2b1886fe31b46d016032243bd539caff
aleksmn <al-mn@yandex.ru>
2023-02-07 16:00:19 +0300

    fix

--- module-2/03.py
+++ module-2/03.py
@@ -8,24 +8,22 @@ book_phones = {'Алексей': 23345, 'Дмитрий': 456433, 'Юлия': 37
 
 # print(book_phones)
 
-for k, v in book_phones.items():
-    print(k, v)
 
-
-
-print("-------------------")
 name = input('Имя: ')
 phone = input('Телефон: ')
 
 
-
 if name and phone:
     book_phones[name] = phone
+    # print('----------------------')
     for key in book_phones:
         print(f'{key}: {book_phones[key]}' )
 
-elif name and name in book_phones:
+elif name in book_phones:
     print(book_phones[name])
 
 else:
     print('Нет в телефонной книге')
+
+
+
