364ec43f2fb4650b6332333a7951b36eb9ac4e16
aleksmn <al-mn@yandex.ru>
2023-02-14 15:34:31 +0300

    fix

--- module-2/phonebook/main.py
+++ module-2/phonebook/main.py
@@ -8,18 +8,26 @@ os.system('clear')
 
 # Получаем данные из файла
 
-book_phones = {}
+# book_phones = {}
 
-with open('book.txt', 'r', encoding='utf-8') as f:
-    lines = f.readlines()
+# with open('book.txt', 'r', encoding='utf-8') as f:
+#     lines = f.readlines()
     
-for line in lines:
-    line = line.split(';')
-    name = line[0].strip()
-    phone = line[1].strip()
-    book_phones[name] = phone
+# for line in lines:
+#     line = line.split(';')
+#     name = line[0].strip()
+#     phone = line[1].strip()
+#     book_phones[name] = phone
 
 # print(book_phones)
+
+book_phones = {
+  'Квам-Дамн': '-79899899889',
+  'Лук Скамворкер': '112',
+  'Петард Вейпер': '1',
+  'Лия Моргала': '+09998765432',
+  'Эдуард Скамворкер': '0'
+}
 
 
 action = input(
@@ -43,7 +51,9 @@ elif action == '2':
 
     phone = input('Телефон: ')
     book_phones[name] = phone
-    print(book_phones)
+    # Вывод телефонной книги
+    for k, v in book_phones.items():
+        print(f'{k}: {v}')
 
 # Изменение номера
 elif action == '3':
@@ -53,15 +63,30 @@ elif action == '3':
         quit()
     phone = input('Телефон: ')
     book_phones[name] = phone
-    print(book_phones)
+
+    # Вывод телефонной книги
+    for k, v in book_phones.items():
+        print(f'{k}: {v}')
+
 
 elif action == '4':
     name = input('Имя для удаления: ')
     if name in book_phones:
         book_phones.pop(name)
-        print(book_phones)
+        # Вывод телефонной книги
+        for k, v in book_phones.items():
+            print(f'{k}: {v}')
     else:
         print("Нет в телефонной книге")
+
+
+
+
+
+
+
+
+
 
 
 # Переписываем файл
