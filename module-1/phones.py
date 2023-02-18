aa9a9ed2a2e64448bb1a669509e18bd1bbae60b5
aleksmn <al-mn@yandex.ru>
2023-02-18 10:53:08 +0300

    add read file

--- module-1/phones.py
+++ module-1/phones.py
@@ -8,15 +8,31 @@ name = input('Введите имя: ')
 phone = input("Введите номер телефона (11 цифр): ")
 comment = input('Комментарий: ')
 
-# phone = "+7 (" + phone[1:4] + ") " + phone[4:]
 
 phone = f'+7 ({phone[1:4]}) {phone[4:7]}-{phone[7:9]}-{phone[9:]}'
+
+
+# Создаем контакт
 contact = f'{name}; {phone}; {comment}'
 
 print(contact)
 
-print(contact.split(';'))
 
-# Записываем в файл
+# Добавляем контакт в файл
+
 with open('my-contacts.txt', 'a', encoding='utf-8') as f:
     f.write(contact + '\n')
+
+
+
+# Все контакты
+print("*** Все контакты ***")
+
+with open('my-contacts.txt', 'r', encoding='utf-8') as f:
+    lines = f.readlines()
+    for line in lines:
+        print(line, end='')
+
+
+
+
