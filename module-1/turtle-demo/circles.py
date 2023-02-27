942d3aa2610ebc41b562bb8e8a7b8e1c5a7807c8
aleksmn <al-mn@yandex.ru>
2023-02-27 18:59:44 +0300

    fix

--- module-1/turtle-demo/circles.py
+++ module-1/turtle-demo/circles.py
@@ -1,28 +1,45 @@
 import turtle
 import random
 
-turtle.shape()
-turtle.color('red')
-turtle.bgcolor('black')
+turtle.setup(900, 900)
+turtle.shape("turtle")
+turtle.color("#E96479")
+turtle.bgcolor('#4D455D')
 
 
+# Примеры:
+# turtle.circle(100)
 
+# turtle.left(90)
+# turtle.forward(50)
 
 # turtle.dot(100)
-# turtle.forward(100)
-# turtle.circle(50)
 
-### Гипнокруги
 
-# Устанавливаем размеры окна
-turtle.Screen().setup(1920, 1080)
-turtle.speed(50)
+# Гипнокруги
+
+turtle.speed(8)
+turtle.hideturtle()
+# turtle.width(2)
+
 size = 5
 
-for i in range(0, 160):
-    turtle.circle(size)
-    turtle.left(5)
-    size += 10
+for i in range(1, 160):
+    turtle.circle(size * i)
+
+    turtle.left(20)
+
+
+# Гипнокруги
+
+# Устанавливаем размеры окна
+# turtle.speed(50)
+# size = 5
+
+# for i in range(0, 160):
+#     turtle.circle(size)
+#     turtle.left(5)
+#     size += 10
 
 
 turtle.mainloop()
