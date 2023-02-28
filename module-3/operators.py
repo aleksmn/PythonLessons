23367023d8804b8c07d2e123108d28a8f3e7d396
aleksmn <al-mn@yandex.ru>
2023-02-28 17:41:14 +0300

    add functions

--- module-3/operators.py
+++ module-3/operators.py
@@ -1,6 +1,5 @@
-def divide_nums(a, b):
+def divide_nums(a: float, b: float) -> float:
     result = 0
-
     try:
         result = float(a) / float(b)
     except ZeroDivisionError:
@@ -13,3 +12,49 @@ def divide_nums(a, b):
         return result
 
 
+def sum_nums(a: float, b: float) -> float:
+    result = 0
+    try:
+        result = float(a) + float(b)
+    except ValueError:
+        print('Функция работает только с числами')
+    except Exception as e:
+        print(e)
+    finally:
+        return result
+    
+
+def sub_nums(a: float, b: float) -> float:
+    result = 0
+    try:
+        result = float(a) - float(b)
+    except ValueError:
+        print('Функция работает только с числами')
+    except Exception as e:
+        print(e)
+    finally:
+        return result
+    
+
+def multiply_nums(a: float, b: float) -> float:
+    result = 0
+    try:
+        result = float(a) * float(b)
+    except ValueError:
+        print('Функция работает только с числами')
+    except Exception as e:
+        print(e)
+    finally:
+        return result
+
+
+
+
+if __name__ == "__main__":
+
+    print("Тестим функции")
+
+    # Тестируем функции
+    print(sum_nums(4, 6))
+    print(sub_nums(4, 't'))
+    print(multiply_nums(4, 't'))
