5bafd57e6683baaaeee2619da04ce7f80967ce6e
aleksmn <al-mn@yandex.ru>
2023-03-02 19:04:33 +0300

    fix

--- l-20.py
+++ l-20.py
@@ -1,4 +1,3 @@
-from unittest import result
 import skysmart
 
 skysmart.printTitle("Привет!")
--- l-24.py
+++ l-24.py
@@ -2,7 +2,7 @@
 
 def double(*nums: int) -> list:
 
-    return [num**2 for num in nums]
+    return [num*2 for num in nums]
 
 
 print(double(3, 4, 7, 8, 1, 12))
