4d32d5c2d5a6ff0a03e9d87f563a58a3a27b08c0
aleksmn <al-mn@yandex.ru>
2023-03-04 10:26:35 +0300

    init

---
+++ module-1/turtle-demo/abstract_1.py
@@ -0,0 +1,42 @@
+import turtle
+import random
+
+turtle.setup(900, 900)
+turtle.shape("turtle")
+turtle.color("#E96479")
+turtle.bgcolor('#4D455D')
+
+# Перемещаемся на случайную точку для начала рисунка
+turtle.penup()
+turtle.goto(random.randint(-350, 350), random.randint(-350, 350))
+turtle.pendown()
+
+# Напишем функцию для рисования квадрата
+
+
+def draw_square():
+    turtle.begin_fill()
+    # Рисуем квадрат со стороной side
+    side = random.randint(10, 100)
+    for i in range(4):
+        turtle.forward(side)
+        turtle.left(90)
+
+    turtle.end_fill()
+
+
+# Приступаем к созданию картины
+
+n = 10    # Количество квадратов
+
+for i in range(n-1):
+    draw_square()
+    turtle.goto(random.randint(-350, 350), random.randint(-350, 350))
+
+
+draw_square()
+# Прячем черепашку
+turtle.hideturtle()
+
+# Основной цикл
+turtle.mainloop()
