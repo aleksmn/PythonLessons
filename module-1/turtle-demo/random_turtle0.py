7162e5dd65b1630e04ab0fc5255ea970e21ee8d0
aleksmn <al-mn@yandex.ru>
2023-03-09 19:47:38 +0300

    fix

--- module-1/turtle-demo/random_turtle0.py
+++ module-1/turtle-demo/random_turtle0.py
@@ -4,7 +4,7 @@ import random
 turtle.setup(900, 900)
 turtle.hideturtle()
 
-turtle.color("#E96479")
+turtle.color("black")
 # turtle.bgcolor('#4D455D')
 turtle.bgcolor("#E8E2E2")
 turtle.shape('turtle')
@@ -12,29 +12,36 @@ turtle.speed(20)
 # Список цветов
 colors = ['#2B3467', '#804674', '#B3E5BE', '#FF0303', '#E96479', '#F99417', '#5D3891']
 
+turtle.width(4)
+
 
 for i in range(30):
     # Перемещаем черепашку в случайную точку
     # Получаем случайные координаты
-    x = random.randint(-350, 350)
-    y = random.randint(-350, 350)
+    x = random.randint(-300, 300)
+    y = random.randint(-300, 300)
 
     turtle.penup()
-    turtle.goto(x, y)
+
+    size = random.randint(6, 160)
+
+    turtle.goto(x-size/2, y-size/2)
     # Рисуем квадрат!
     turtle.pendown()
 
     # Меняем цвет на случайный из списка
-    turtle.color(random.choice(colors))
+    turtle.fillcolor(random.choice(colors))
 
 
     # Начинаем заливку
     turtle.begin_fill()
 
-    size = random.randint(10, 100)
+    
+
 
     n = 4
-    turtle.left(random.randint(1, 360))
+    #turtle.left(random.randint(1, 360))
+    turtle.setheading(15)
     for i in range(n):
         
         turtle.forward(size)
