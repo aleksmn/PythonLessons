f5fd56a267bb7a59271a176d4010cb5304ead160
aleksmn <al-mn@yandex.ru>
2023-03-12 19:33:37 +0300

    fix

--- module-2/dict_demo.py
+++ module-2/dict_demo.py
@@ -11,12 +11,12 @@
 # print(my_dictionary['один'])
 # print(my_dictionary['два'])
 
-
-
-
-# # print(type(my_dictionary))
-
 # my_dictionary['четыре'] = 'four'
+
+
+# print(type(my_dictionary))
+
+
 
 
 # while True:
@@ -50,17 +50,53 @@
 # Телефонная книга
 # Создаем словарь из двух списков:
 
-names = ['Квам-Дамн', 'Лук Скамворкер', 'Петард Вейпер', 'Лия Моргала', 'Эдуард Скамворкер']
-phones = ['-79899899889', '112', '1', '+09998765432', '0']
+# names = ['Квам-Дамн', 'Лук Скамворкер', 'Петард Вейпер', 'Лия Моргала', 'Эдуард Скамворкер']
+# phones = ['-79899899889', '112', '1', '+09998765432', '0']
 
 
-book_phones = {}
+# book_phones = {}
 
-counter = 0
+# counter = 0
 
-for name in names:
-    book_phones[name] = phones[counter]
-    counter += 1
+# for name in names:
+#     book_phones[name] = phones[counter]
+#     counter += 1
 
 
-print(book_phones)
+# print(book_phones)
+
+
+
+
+
+
+book_phones = {
+    'Квам-Дамн': '-79899899889',
+    'Лук Скамворкер': '112',
+    'Петард Вейпер': '1',
+    'Лия Моргала': '+09998765432',
+    'Эдуард Скамворкер': '0',
+}
+
+
+name = input("Имя: ")
+
+number = input("Номер телефона: ")
+
+
+if name and number:
+    # Добавляем или изменяем запись
+    book_phones[name] = number
+    # Выводим 
+    for key in book_phones:
+        print(f'{key}: {book_phones[key]}')
+
+elif name in book_phones:
+    print(book_phones[name])
+
+else:
+    print('Нет в телефонной книге')
+
+
+
+
