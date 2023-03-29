93b543185f705e799d088f393f35f57952480528
aleksmn <al-mn@yandex.ru>
2023-03-29 17:37:22 +0300

    add sub an super sets

--- module-2/sets_demo.py
+++ module-2/sets_demo.py
@@ -6,47 +6,64 @@ prog = {'CSS', 'Python', 'HTML', 'C++', 'Go'}
 lang = {'Python', 'Java','Go', 'CSS', 'Rust'}
 
 
+prog.add('C#')
+print(prog)
 
-# prog.add('C#')
-# print(prog)
-
-# lang.remove('Go')
-# print(lang)
+lang.remove('Go')
+print(lang)
 
 # Union (объединение)
-# my_stack_1 = prog.union(lang)
-# my_stack_2 = prog | lang
+my_stack_1 = prog.union(lang)
+my_stack_2 = prog | lang
 
-# print('Union:', my_stack_1)
-# print('Union:', my_stack_2)
+print('Union:', my_stack_1)
+print('Union:', my_stack_2)
 
 # Intersection (пересечение)
-# my_stack_1 = prog.intersection(lang)
-# my_stack_2 = prog & lang
+my_stack_1 = prog.intersection(lang)
+my_stack_2 = prog & lang
 
-# print('Intersection:', my_stack_1)
-# print('Intersection:', my_stack_2)
+print('Intersection:', my_stack_1)
+print('Intersection:', my_stack_2)
 
 
 # Difference (разность)
-# my_stack_1 = prog.difference(lang)
-# my_stack_2 = prog - lang
+my_stack_1 = lang.difference(prog)
+my_stack_2 = lang - prog
 
-# print('Difference:', my_stack_1)
-# print('Difference:', my_stack_2)
+print('Difference:', my_stack_1)
+print('Difference:', my_stack_2)
 
 
 # Symmetric difference (Симметричная разность)
-# my_stack_1 = prog.symmetric_difference(lang)
-# my_stack_2 = prog ^ lang
+my_stack_1 = prog.symmetric_difference(lang)
+my_stack_2 = prog ^ lang
 
-# print('Symmetric difference:', my_stack_1)
-# print('Symmetric difference:', my_stack_2)
+print('Symmetric difference:', my_stack_1)
+print('Symmetric difference:', my_stack_2)
 
 
 
 
 # Update
-# print(prog)
-# prog.update(lang)
-# print(prog)
+print(prog)
+prog.update(lang)
+print(prog)
+
+
+
+# Is Subset
+
+x = {"a", "b", "c"}
+y = {'f', "a", "c", "d", "e", "b"}
+print('Первое множество является подмножеством второго?')
+print(x.issubset(y)) 
+
+
+
+# Is Superset
+
+x = {"f", "e", "d", "c", "b", "a"}
+y = {"a", "b", "c"}
+print('Второе множество является подмножеством первого?')
+print(x.issuperset(y))
