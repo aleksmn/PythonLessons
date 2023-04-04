f6899bd0d01186d2efa10cfe93364d04680adefa
aleksmn <al-mn@yandex.ru>
2023-04-04 16:44:55 +0300

    fix

--- module-3/04.py
+++ module-3/04.py
@@ -1,7 +1,49 @@
-# Подсказки типов
+
+# Необязательный аргумент
+def say_hello(name: str = 'Пользователь'):
+    return "Привет " + name + "!"
 
 
-my_number: int = 8
-  
 
-print(my_number)
+print(say_hello("Дима"))
+print(say_hello())
+
+
+
+def get_sum(*args):
+
+    result = 0
+
+    for num in args:
+        result += num
+
+    return result
+
+
+print(get_sum(2, 6, 1, 8, 9))
+
+
+def create_user(**params):
+    
+    return(params)
+
+
+print(create_user(fname="Виталий", lname="Иванов", job='Учитель'))
+
+
+
+# До / можно записывать только позиционные аргументы
+
+def diff(num1, num2, /):
+    return num1 - num2
+
+
+print(diff(3, 5))
+
+
+
+
+
+
+
+
