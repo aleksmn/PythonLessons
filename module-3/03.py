a51cbd66523b7d6a70c0ee67d9900c87577bbdaa
aleksmn <al-mn@yandex.ru>
2023-04-07 12:00:28 +0300

    fix

--- module-3/03.py
+++ module-3/03.py
@@ -1,11 +1,17 @@
 
 
-# new_data = map(int, ['14', '51', '23', '43', '87'])
-# new_data = list(new_data)
-# print(new_data)   # [14, 51, 23, 43, 87]
+# Функция map
+
+new_data = map(int, ['14', '51', '23', '43', '87'])
+
+new_data = list(new_data)
+print(new_data)   # [14, 51, 23, 43, 87]
 
 
-# new_data = map(lambda a: int(a)**2, ['14', '51', '23', '43', '87'])
-# new_data = list(new_data)
-# print(new_data)   # [196, 2601, 529, 1849, 7569]
+
+# Использование анонимной функции (lambda)
+
+new_data = map(lambda a: int(a)**2, ['14', '51', '23', '43', '87'])
+new_data = list(new_data)
+print(new_data)   # [196, 2601, 529, 1849, 7569]
  
--- test.py
+++ test.py
@@ -1,11 +1,21 @@
-a = input('Введи текст: ')
+def is_sublist(larger, smaller):
+    n = len(larger)
+    m = len(smaller)
+    
+    if m > n:
+        return False
+    
+    for i in range(n - m + 1):
+        if larger[i:i+m] == smaller:
+            return True
+        
+    return False
 
-# Только строчные буквы
-# a = a.lower()
 
-# Только Заглавные буквы
-a = a.upper()
 
-print('*' * (len(a) + 4))
-print('* ' + a + ' *')
-print('*' * (len(a) + 4))
+
+list_1= [4, 8, 1, 0, 12,  6]
+list_2= [1, 0, 12]
+
+
+print(is_sublist(list_1, list_2))
