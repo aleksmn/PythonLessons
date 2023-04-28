0dd88bc54d110bae52946351f6eaf7d9e5336306
aleksmn <al-mn@yandex.ru>
2023-04-28 18:01:20 +0300

    add func

--- module-5/rest/main.py
+++ module-5/rest/main.py
@@ -6,4 +6,22 @@ def receipt():
 
 
 
-receipt()
+def restaurant_data():
+    restaurant = {"details": []}
+    with open("restaurant.txt", 'r', encoding='utf-8') as f:
+        for item in f.read().split('***\n'):
+            item_list = item.split('\n')
+
+            restaurant['details'].append({
+                "administrator":item_list[0],
+                "workers":item_list[1].split(", "),
+                "turnover":item_list[2],
+                "revenue":item_list[3],
+                "tips":item_list[4],
+            })
+
+    # Вывод словаря
+    return restaurant
+
+
+print(restaurant_data())
