6cb6ef40ca72c154e4c71714989fd8fecd9e853c
aleksmn <al-mn@yandex.ru>
2023-05-10 13:50:09 +0300

    init

---
+++ module-5/02.py
@@ -0,0 +1,102 @@
+
+class Airplane:
+    engines = int(input('Двигатели: '))
+    seats = int(input('Посадочные места: '))
+
+    print('Самолет собран!')
+
+
+plane1 = Airplane()
+plane2 = Airplane()
+plane3 = Airplane()
+
+
+print(plane1.engines)
+print(plane2.engines)
+print(plane3.engines)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# class Airplane:
+#     def __init__(self):
+#         '''Конструктор класса'''
+#         self.engines = int(input('Двигатели: '))
+#         self.seats = int(input('Посадочные места: '))
+
+#         print('Самолет собран!')    
+
+
+# plane1 = Airplane()
+# plane2 = Airplane()
+# plane3 = Airplane()
+
+
+# print(plane1.engines, plane1.seats)
+# print(plane2.engines, plane2.seats)
+# print(plane3.engines, plane3.seats)
+
+
+
+
+
+
+
+
+# class Airplane:
+#     def __init__(self, engines=1, seats=2):
+#         '''Конструктор класса'''
+#         self.engines = engines
+#         self.seats = seats
+
+#         print('Самолет собран!')    
+
+
+# plane1 = Airplane(engines=3, seats=5)
+# plane2 = Airplane()
+# plane3 = Airplane()
+
+
+# print(plane1.engines, plane1.seats)
+# print(plane2.engines, plane2.seats)
+# print(plane3.engines, plane3.seats)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
