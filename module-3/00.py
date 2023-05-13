47ed05ed944cb688785998c8ce75cccfcea14415
aleksmn <al-mn@yandex.ru>
2023-05-13 13:54:00 +0300

    fix

--- module-3/00.py
+++ module-3/00.py
@@ -1,7 +1,29 @@
 
+# Объявляем новую функцию
+# с параметром name
 
-def sayHello():
-    print("Привет!")
+def sayHello(name):
+    print("Привет ", name) 
+
+
+# Вызов функции
+# с аргументом
+sayHello("Михаил")
 
 
 
+
+
+
+def getSum(a=0, b=0):
+
+    return a + b
+
+
+
+res = getSum(7, 8) * getSum(10, 42) 
+
+
+print(res)
+
+print(getSum(234,345))
