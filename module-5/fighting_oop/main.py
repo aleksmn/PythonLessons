d98fcad7ca17ffb8e317f8ef515ac804f9fdab04
aleksmn <al-mn@yandex.ru>
2023-05-14 14:45:52 +0300

    add methods

--- module-5/fighting_oop/main.py
+++ module-5/fighting_oop/main.py
@@ -1,5 +1,12 @@
-# Пример решения задания
+import random
+
 class Person:
+    role = {
+        '1': 'Воин',
+        '2': 'Лучник',
+        '3': 'Маг',
+    }
+
     classes = {
         'Воин': {
             'здоровье': 100,
@@ -28,21 +35,36 @@ class Person:
         }
     }
 
-    def __init__(self, name, person_class, is_enemy):
+    def __init__(self, name, is_enemy):
         self.name = name
-        self.person_class = self.classes[person_class]
-        self.health = self.person_class['здоровье']
-        self.attack = self.person_class['атака']
-        self.defence = self.person_class['защита']
+        self.is_enemy = is_enemy
+        self.person_class = self.get_person_class()
+        self.person_class_dict = self.classes[self.person_class]
+        self.health = self.person_class_dict['здоровье']
+        self.attack = self.person_class_dict['атака']
+        self.defence = self.person_class_dict['защита']
         self.money = 1000.0
-        self.skills = self.person_class['навыки']
+        self.skills = self.person_class_dict['навыки']
         self.inventory = ['яблоко', 'шаурма']
         self.is_alive = True
-        self.is_enemy = is_enemy
+
+        print(f"{self.name} - {self.person_class}.")
+        print("У него такие характеристики:")
+        print(f" здоровье - {self.health},\n атака - {self.attack},\n защита - {self.defence}.")
 
 
-player = Person(name='Вася', person_class='Воин', is_enemy=False)
-enemy = Person(name='Летающий чугун', person_class='Лучник', is_enemy=True)
+    def get_person_class(self):
+        if self.is_enemy:
+            random_choice = random.choice(list(self.role.keys()))
+            # random_choice = random.choice('123')
+            return self.role[random_choice]
+        else:
+            choice = input(f"Введите роль: 1-Воин, 2-Лучник, 3-Маг\n")
+            return self.role[choice]
 
 
-print(player.name)
+player = Person('Олег', is_enemy=False)
+ogr = Person('Комочек тьмы', is_enemy=True)
+
+
+# print(player.name)
