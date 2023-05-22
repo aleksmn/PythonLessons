4c8afd00903d3719ce8429a81d6ff12160191982
aleksmn <al-mn@yandex.ru>
2023-05-22 15:26:29 +0300

    add func

--- module-4/rest/main.py
+++ module-4/rest/main.py
@@ -5,26 +5,55 @@ def receipt():
             print(line.strip())
 
 
-def restaurant_data():
-    restaurant = {"details": []}
-    with open("receipt.txt", 'r', encoding='utf-8') as f:
-        for item in f.read().split('***\n'):
-            item_list = item.split('\n')
 
-            restaurant['details'].append({
-                "administrator":item_list[0],
-                "workers":item_list[1].split(", "),
-                "turnover":item_list[2],
-                "revenue":item_list[3],
-                "tips":item_list[4],
-            })
+def show_food():
+    food = []
+    with open('receipt.txt', 'r', encoding='utf-8') as f:    
+        # Вывод построчно
+        for line in f:
+            # print(line.split())
+            food.append(line.split()[0])
 
-    # Вывод словаря
-    return restaurant
+    print(food) 
 
 
 
-if __name__ == "__main__":
-    receipt()
+show_food()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def restaurant_data():
+#     restaurant = {"details": []}
+#     with open("receipt.txt", 'r', encoding='utf-8') as f:
+#         for item in f.read().split('***\n'):
+#             item_list = item.split('\n')
+
+#             restaurant['details'].append({
+#                 "administrator":item_list[0],
+#                 "workers":item_list[1].split(", "),
+#                 "turnover":item_list[2],
+#                 "revenue":item_list[3],
+#                 "tips":item_list[4],
+#             })
+
+#     # Вывод словаря
+#     return restaurant
+
+
+
+# if __name__ == "__main__":
+#     receipt()
 
     # print(restaurant_data()) 
