1f928b037e862a8b3b7ed2f6cf8457c112dbb175
aleksmn <al-mn@yandex.ru>
2023-05-22 15:34:31 +0300

    add file

--- module-4/rest/main.py
+++ module-4/rest/main.py
@@ -18,42 +18,24 @@ def show_food():
 
 
 
-show_food()
+
+def restaurant_data():
+    restaurant = {"details": []}
+    with open("receipt.txt", 'r', encoding='utf-8') as f:
+        for item in f.read().split('***\n'):
+            item_list = item.split('\n')
+
+            restaurant['details'].append({
+                "administrator":item_list[0],
+                "workers":item_list[1].split(", "),
+                "turnover":item_list[2],
+                "revenue":item_list[3],
+                "tips":item_list[4],
+            })
+
+    # Вывод словаря
+    return restaurant
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-# def restaurant_data():
-#     restaurant = {"details": []}
-#     with open("receipt.txt", 'r', encoding='utf-8') as f:
-#         for item in f.read().split('***\n'):
-#             item_list = item.split('\n')
-
-#             restaurant['details'].append({
-#                 "administrator":item_list[0],
-#                 "workers":item_list[1].split(", "),
-#                 "turnover":item_list[2],
-#                 "revenue":item_list[3],
-#                 "tips":item_list[4],
-#             })
-
-#     # Вывод словаря
-#     return restaurant
-
-
-
-# if __name__ == "__main__":
-#     receipt()
-
-    # print(restaurant_data()) 
