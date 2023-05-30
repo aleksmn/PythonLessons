935e1f3fa131d9ebff2db7c7cedafb3b4458c255
aleksmn <al-mn@yandex.ru>
2023-05-30 17:55:19 +0300

    fix

--- module-5/04.py
+++ module-5/04.py
@@ -1,56 +1,52 @@
-class Animal:
-    def run(self):
-        print("Бегаю!!!")
+# class Cat():
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
 
-    def sleep(self):
-        print("Сплю...")
+#     def __str__(self) -> str:
+#         return f"Кошка по имени {self.name}. Возраст: {self.age}."
 
-
-
-class Cat():
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def __str__(self) -> str:
-        return f"Кошка по имени {self.name}. Возраст: {self.age}."
-
-    def speak(self):
-        print("Мяу-мяу!")
-
-
-class Dog():
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
-
-    def __str__(self) -> str:
-        return f"Собака по имени {self.name}. Возраст: {self.age}."
-
-    # Полиморфизм: один метод - разные действия
-    def speak(self):
-        print("Гав-гав!")
+#     def speak(self):
+#         print("Мяу-мяу!")
 
 
 
+# class Dog():
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
 
-my_cat = Cat("Мурка", 2.5)
-my_dog = Dog("Барбос", 4)
+#     def __str__(self) -> str:
+#         return f"Собака по имени {self.name}. Возраст: {self.age}."
 
-print(my_cat)
-my_cat.speak()
-
-
-print(my_dog)
-my_dog.speak()
+#     # Полиморфизм: название метода одинаковое - разные действия
+#     def speak(self):
+#         print("Гав-гав!")
 
 
 
 
 
-# for animal in (my_cat, my_dog):
-#     print(animal)
-#     animal.speak()
+# my_cat = Cat("Мурка", 2.5)
+
+# print(my_cat.sleep())
+
+
+
+# my_dog = Dog("Барбос", 4)
+
+# print(my_cat)
+# my_cat.speak()
+
+
+# print(my_dog)
+# my_dog.speak()
+
+
+
+
+
+
 
 
 
@@ -73,7 +69,7 @@ my_dog.speak()
 
 
 
-# class Cat():
+# class Cat(Animal):
 
 #     def __init__(self, name, age):
 #         Animal.__init__(self, name, age)
@@ -86,7 +82,7 @@ my_dog.speak()
 #         print("Мяу-мяу!")
 
 
-# class Dog():
+# class Dog(Animal):
 #     def __init__(self, name, age):
 #         Animal.__init__(self, name, age)
 
@@ -105,7 +101,14 @@ my_dog.speak()
 
 # print(my_cat)
 # my_cat.speak()
-
+# my_cat.sleep()
 
 # print(my_dog)
 # my_dog.speak()
+# my_dog.sleep()
+
+
+
+# for animal in (my_cat, my_dog):
+#     print(animal)
+#     animal.speak()
