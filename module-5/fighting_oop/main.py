e276eea5027a25124c2139c2f4d5c0a0598b76f2
aleksmn <al-mn@yandex.ru>
2023-06-02 17:05:46 +0300

    add KeyboardInterrupt exceptuion

--- module-5/fighting_oop/main.py
+++ module-5/fighting_oop/main.py
@@ -2,11 +2,18 @@ from Player import Player
 from Enemy import Enemy
 
 
-
-if __name__ == "__main__":
+def main():
 
     player = Player()
-
     enemy = Enemy()
 
     player.fight_for_the_win(player, enemy)
+
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("Выход из игры...")
+        quit()
