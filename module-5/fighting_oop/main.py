15f2f39558222ac1d2a462f28ccff26c1e7214a0
aleksmn <al-mn@yandex.ru>
2023-06-02 16:51:52 +0300

    fix

--- module-5/fighting_oop/main.py
+++ module-5/fighting_oop/main.py
@@ -1,9 +1,12 @@
 from Player import Player
 from Enemy import Enemy
-from Person import Person
 
-player = Player()
 
-enemy = Enemy()
 
-player.fight_for_the_win(player, enemy)
+if __name__ == "__main__":
+
+    player = Player()
+
+    enemy = Enemy()
+
+    player.fight_for_the_win(player, enemy)
