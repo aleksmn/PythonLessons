186584ff5c210a074457048600ed2bdee00cc174
aleksmn <al-mn@yandex.ru>
2023-06-09 17:29:55 +0300

    fix

--- l-17.py
+++ l-17.py
@@ -6,3 +6,25 @@ def print10(s):
 
 
 print10("Привет")
+
+
+
+
+
+
+
+n = input("Введите число: ")
+
+h = len(n) // 2
+
+summa1 = 0
+summa2 = 0
+
+for i in range(h):
+    summa1 += int(n[i])
+    summa2 += int(n[-(i + 1)])
+
+
+# После цикла
+print(summa1)
+print(summa2)
