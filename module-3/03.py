ff8753eacc0443045ac7e7209296bd26561b4a7c
aleksmn <al-mn@yandex.ru>
2023-06-17 12:54:26 +0300

    fix

--- module-3/03.py
+++ module-3/03.py
@@ -1,17 +1,54 @@
 
 
-# Функция map
 
-new_data = map(int, ['14', '51', '23', '43', '87'])
+def sayHello(age, name="Пользователь"):
 
-new_data = list(new_data)
-print(new_data)   # [14, 51, 23, 43, 87]
-
+    print("Привет," , name)
+    print("Твой возраст", age)
 
 
-# Использование анонимной функции (lambda)
+sayHello(12, "Дима")
+sayHello(10)
 
-new_data = map(lambda a: int(a)**2, ['14', '51', '23', '43', '87'])
-new_data = list(new_data)
-print(new_data)   # [196, 2601, 529, 1849, 7569]
+sayHello(name='Мария', age=14)
+
+
+# Аргументы переменной длины
+
+def multiply(*args):
+  result = 1
+  for arg in args:
+    result *= arg
+  return result
+
+
+
+print(multiply(1, 5, 8, 8))
+
+
+
+
+
+
+
+
+
+
+
+
+
+# # Функция map
+
+# new_data = map(int, ['14', '51', '23', '43', '87'])
+
+# new_data = list(new_data)
+# print(new_data)   # [14, 51, 23, 43, 87]
+
+
+
+# # Использование анонимной функции (lambda)
+
+# new_data = map(lambda a: int(a)**2, ['14', '51', '23', '43', '87'])
+# new_data = list(new_data)
+# print(new_data)   # [196, 2601, 529, 1849, 7569]
  
