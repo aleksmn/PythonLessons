7d12e6565e1b1636e57cfa660570af1f42022dd9
aleksmn <al-mn@yandex.ru>
2023-06-27 17:55:46 +0300

    add

--- big-O.py
+++ big-O.py
@@ -19,35 +19,61 @@ print(getFirst([9, 5, 6]))
 
 # функция нахождения суммы массивы
 
-def getSum(myList):
-    sum = 0
+# def getSum(myList):
+#     sum = 0
 
-    for i in myList:
-        sum += i
+#     for i in myList:
+#         sum += i
 
-    return sum
+#     return sum
 
-print(getSum([5, 8, 12]))
+# print(getSum([5, 8, 12]))
 
 
 
 # Квадратичная временная сложность 
 # O(n^2)
 
-myList = [[1, 4], [5, 6], [9, 5], [14, 3]]
+# myList = [[1, 4], [5, 6], [9, 5], [14, 3]]
 
-def getSum2(myList):
-    sum = 0
-    for row in myList:
-        for item in row:
-            sum += item
-    return sum
+# def getSum2(myList):
+#     sum = 0
+#     for row in myList:
+#         for item in row:
+#             sum += item
+#     return sum
 
-print(getSum2)
+# print(getSum2)
 
 
 
 # Логарифмическая временная сложность 
 # O(log n)
+
+
+def searchBinary(myList, item):
+    first = 0
+    last = len(myList) - 1
+    isFound = False
+
+    while (first <= last and not isFound):
+        # Индекс центрального элемента
+        mid = (first + last) // 2
+        if myList[mid] == item:
+            isFound = True
+        else:
+            if item < myList[mid]:
+                last = mid - 1
+            else:
+                first = mid + 1
+
+    return isFound
+
+
+
+a = [3, 8, 15, 22, 31, 40, 18]
+
+print(searchBinary(a, 15))
+
 
 
