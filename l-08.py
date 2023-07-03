eec6272ec91c2ab2b8d258cd7402342d82d76531
aleksmn <al-mn@yandex.ru>
2023-07-03 19:55:10 +0300

    добавил решение

--- l-08.py
+++ l-08.py
@@ -106,3 +106,20 @@
 # else:
 #     print(0)
 
+
+
+
+# Ход ферзя
+
+# x1 = int(input("x1 = "))
+# y1 = int(input("y1 = "))
+
+
+# x2 = int(input("x2 = "))
+# y2 = int(input("y2 = "))
+
+
+# if x1 - y1 == x2 - y2 or (x1 == x2 or y1 == y2):
+#     print(1)
+# else:
+#     print(0)
