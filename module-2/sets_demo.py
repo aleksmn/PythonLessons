cc391081de067ed6bffe958adf99436d22dab4a9
aleksmn <al-mn@yandex.ru>
2023-07-21 14:15:55 +0300

    add tasks

--- module-2/sets_demo.py
+++ module-2/sets_demo.py
@@ -5,12 +5,27 @@ prog = {'CSS', 'Python', 'HTML', 'C++', 'Go'}
 
 lang = {'Python', 'Java','Go', 'CSS', 'Rust'}
 
-
 prog.add('C#')
-print(prog)
+print('SET prog:', prog)
 
 lang.remove('Go')
-print(lang)
+print('SET lang:', lang)
+'''
+
+1. Создать множество, которое включает значения из первого и второго множества.
+2. Вывести только элементы, которые встречаются в обоих множествах.
+3. Удалить из первого множества все элементы, которые есть во втором множестве.
+4. Вывести из двух множеств элементы, которые встречаются в одном, но не встречаются в другом множестве.
+
+
+'''
+
+
+
+
+
+
+
 
 # Union (объединение)
 my_stack_1 = prog.union(lang)
@@ -19,7 +34,8 @@ my_stack_2 = prog | lang
 print('Union:', my_stack_1)
 print('Union:', my_stack_2)
 
-# Intersection (пересечение)
+
+# # Intersection (пересечение)
 my_stack_1 = prog.intersection(lang)
 my_stack_2 = prog & lang
 
@@ -27,7 +43,7 @@ print('Intersection:', my_stack_1)
 print('Intersection:', my_stack_2)
 
 
-# Difference (разность)
+# # Difference (разность)
 my_stack_1 = lang.difference(prog)
 my_stack_2 = lang - prog
 
@@ -35,7 +51,8 @@ print('Difference:', my_stack_1)
 print('Difference:', my_stack_2)
 
 
-# Symmetric difference (Симметричная разность)
+
+# # Symmetric difference (Симметричная разность)
 my_stack_1 = prog.symmetric_difference(lang)
 my_stack_2 = prog ^ lang
 
@@ -45,25 +62,29 @@ print('Symmetric difference:', my_stack_2)
 
 
 
-# Update
+# # Update
 print(prog)
 prog.update(lang)
 print(prog)
 
 
 
-# Is Subset
-
-x = {"a", "b", "c"}
-y = {'f', "a", "c", "d", "e", "b"}
-print('Первое множество является подмножеством второго?')
-print(x.issubset(y)) 
 
 
 
-# Is Superset
 
-x = {"f", "e", "d", "c", "b", "a"}
-y = {"a", "b", "c"}
-print('Второе множество является подмножеством первого?')
-print(x.issuperset(y))
+# # Is Subset
+
+# x = {"a", "b", "c"}
+# y = {'f', "a", "c", "d", "e", "b"}
+# print('Первое множество является подмножеством второго?')
+# print(x.issubset(y)) 
+
+
+
+# # Is Superset
+
+# x = {"f", "e", "d", "c", "b", "a"}
+# y = {"a", "b", "c"}
+# print('Второе множество является подмножеством первого?')
+# print(x.issuperset(y))
