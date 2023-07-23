02ab9471f98123504b12e1751c0487657488fc4e
aleksmn <al-mn@yandex.ru>
2023-07-23 19:43:42 +0300

    add tasks

--- tasks/03-loops.py
+++ tasks/03-loops.py
@@ -168,3 +168,39 @@ for digit1 in range(1, 4):
 '''
 
 # Напишите программу, которая найдет все трехзначные простые числа.
+
+
+
+
+
+# Напишите программу, которая запрашивает у пользователя число 
+# и вычисляет сумму его цифр с использованием цикла while.
+
+
+number = int(input("Введите число: "))
+sum_of_digits = 0
+
+while number > 0:
+    digit = number % 10
+    sum_of_digits += digit
+    number //= 10
+
+print("Сумма цифр числа равна:", sum_of_digits)
+
+
+
+
+# Напишите программу, которая запрашивает у пользователя число и находит сумму всех чисел от 1 до 100, 
+# кратных этому числу, с использованием цикла while.
+
+
+number = int(input("Введите число: "))
+sum_of_multiples = 0
+counter = 1
+
+while counter <= 100:
+    if counter % number == 0:
+        sum_of_multiples += counter
+    counter += 1
+
+print("Сумма всех чисел, кратных", number, ", от 1 до 100 равна:", sum_of_multiples)
