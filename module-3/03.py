e5f6a0cb7248ad9f2a38b763c87db350b54d6e98
aleksmn <al-mn@yandex.ru>
2023-08-02 18:48:25 +0300

    add examples

--- module-3/03.py
+++ module-3/03.py
@@ -1,29 +1,34 @@
 
+# # Функция с необязательным аргументом
+# def sayHello(age, name="Пользователь"):
+
+#     print("Привет," , name)
+#     print("Возраст:", age)
 
 
-def sayHello(age, name="Пользователь"):
-
-    print("Привет," , name)
-    print("Твой возраст", age)
-
-
-sayHello(12, "Дима")
-sayHello(10)
-
-sayHello(name='Мария', age=14)
-
-
-# Аргументы переменной длины
-
-def multiply(*args):
-  result = 1
-  for arg in args:
-    result *= arg
-  return result
+# # sayHello(21)
+# # sayHello(18, name="Дима")
+# # # Именованные аргументы
+# # sayHello(age=10, name="Дима")
+# # # Позиционные аргументы
+# # sayHello(22)
 
 
 
-print(multiply(1, 5, 8, 8))
+# # sayHello(name='Мария', age=14)
+
+
+# # # Аргументы переменной длины
+
+# def multiply(*args):
+#   result = 1
+#   for arg in args:
+#     result *= arg
+#   return result
+
+
+
+# print(multiply(1, 5, 8, 8, 11))
 
 
 
@@ -38,17 +43,34 @@ print(multiply(1, 5, 8, 8))
 
 
 # # Функция map
+# Исходные данные (список строк, которые нужно преобразовать с числа)
+nums =  ['14', '51', '23', '43', '87']
 
-# new_data = map(int, ['14', '51', '23', '43', '87'])
+new_data = list(map(int, nums))
 
-# new_data = list(new_data)
-# print(new_data)   # [14, 51, 23, 43, 87]
-
+print(new_data)   # [14, 51, 23, 43, 87]
 
 
-# # Использование анонимной функции (lambda)
+# Используем свою функцию в функции map
+# def get_square(num):
+#   return int(num)**2
 
-# new_data = map(lambda a: int(a)**2, ['14', '51', '23', '43', '87'])
-# new_data = list(new_data)
-# print(new_data)   # [196, 2601, 529, 1849, 7569]
- 
+# new_data = list(map(get_square, nums))
+# print(new_data) 
+
+
+
+# Используем анонимную функцию (lambda)
+
+new_data = list(map(lambda a: int(a)**2, nums))
+print(new_data) 
+
+
+
+# Фильтруем данные с помощью функции filter
+
+numbers = [-1, -2, 5, 4, -1, 100, 99]
+
+numbers = list(filter(lambda a: a > 0, numbers))
+
+print(numbers)
