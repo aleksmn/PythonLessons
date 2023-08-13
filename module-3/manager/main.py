99f8a77325f2c638e9b84b72d617b506211ae4c9
aleksmn <al-mn@yandex.ru>
2023-08-13 10:53:44 +0300

    fix import

--- module-3/manager/main.py
+++ module-3/manager/main.py
@@ -1,4 +1,4 @@
-from tkinter import *
+import tkinter as tk
 
 
 def move_task(event, source_list, target_list):
@@ -9,7 +9,7 @@ def move_task(event, source_list, target_list):
         print(task)
         # Перемещение задачи из одной доски в другую
         source_list.delete(source_list.curselection())
-        target_list.insert(END, task)
+        target_list.insert("end", task)
     except Exception as e:
         # print("Ошибка ", e)
         print("Listbox пуст!")
@@ -18,17 +18,17 @@ def create_task(event):
     # Добавление новой задачи в список "To Do"
     task = entry.get()
     if task:
-        todo_list.insert(END, task)
-        entry.delete(0, END)
+        todo_list.insert("end", task)
+        entry.delete(0, "end")
 
 # Создание главного окна
-root = Tk()
+root = tk.Tk()
 root.title("Kanban Board")
 
 # Создание списков задач
-todo_list = Listbox(root, height=10, width=30)
-in_progress_list = Listbox(root, height=10, width=30)
-done_list = Listbox(root, height=10, width=30)
+todo_list = tk.Listbox(root, height=10, width=30)
+in_progress_list = tk.Listbox(root, height=10, width=30)
+done_list = tk.Listbox(root, height=10, width=30)
 
 # Расположение списков на доске
 todo_list.grid(row=0, column=0, padx=10, pady=10)
@@ -36,13 +36,13 @@ in_progress_list.grid(row=0, column=1, padx=10, pady=10)
 done_list.grid(row=0, column=2, padx=10, pady=10)
 
 # Создание виджетов интерфейса
-add_label = Label(root, text="Add Task:")
+add_label = tk.Label(root, text="Add Task:")
 add_label.grid(row=1, column=0, pady=5)
 
-entry = Entry(root, width=30)
+entry = tk.Entry(root, width=30)
 entry.grid(row=1, column=1, pady=5)
 
-add_button = Button(root, text="Add", width=10)
+add_button = tk.Button(root, text="Add", width=10)
 add_button.grid(row=1, column=2, pady=5)
 add_button.bind("<Button-1>", create_task)
 
