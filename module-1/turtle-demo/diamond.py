2ffb526675e6cf767aebcb429fcfe7599c7ed1a4
aleksmn <al-mn@yandex.ru>
2023-08-15 19:13:59 +0300

    fix

--- module-1/turtle-demo/diamond.py
+++ module-1/turtle-demo/diamond.py
@@ -1,15 +1,21 @@
 import turtle
 
+
+n = int(input("Введите число: "))
+
 turtle.setup(900, 900)
 turtle.shape("turtle")
 turtle.color("#6527BE")
 turtle.bgcolor('#A7EDE7')
 
-for i in range(0, 10):
-	turtle.right(36)
-	for i in range(0, 5):
+for k in range(n*2):
+	turtle.left(180/n)
+	for i in range(0, n):
 		turtle.forward(100)
-		turtle.right(72)
+		turtle.right(360/n)
 
+
+
+# После цикла
 turtle.hideturtle()
 turtle.mainloop()
