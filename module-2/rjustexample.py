ef3010bb9daaeb452f66703f7e1acdbbfa67eb0d
aleksmn <al-mn@yandex.ru>
2023-08-20 16:10:07 +0300

    fix

--- module-2/rjustexample.py
+++ module-2/rjustexample.py
@@ -11,6 +11,8 @@ print('-'*42)
 for k, v in my_dictionary.items():
     output = f'{k} : {v}'
 
-    print(f"|{output.rjust(40, ' ')}|")
+    print(f"|{output.ljust(40, ' ')}|")
+
+    # print(f'|{k.ljust(20)}{v.just(20)}|')
 
 print('-'*42)
