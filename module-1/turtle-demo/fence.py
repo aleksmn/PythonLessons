8cb22abb741b7045dc9a735870b416520a721614
aleksmn <al-mn@yandex.ru>
2023-08-25 12:12:34 +0300

    fix

--- module-1/turtle-demo/fence.py
+++ module-1/turtle-demo/fence.py
@@ -1,7 +1,15 @@
 import turtle
 
+turtle.color("white")
+turtle.bgcolor("black")
+
 turtle.speed(10)
-for i in range(0, 5):
+
+turtle.penup()
+turtle.back(400)
+turtle.pendown()
+
+for i in range(0, 40):
     turtle.forward(10)
     turtle.left(90)
     turtle.forward(30)
@@ -11,5 +19,9 @@ for i in range(0, 5):
     turtle.forward(30)
     turtle.left(90)
 
+turtle.forward(10)
+
+# Спрятать черепашку
+turtle.hideturtle()
 
 turtle.mainloop()
