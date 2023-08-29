5a57ad88fa56641a404c540ef43d00d472f422f1
aleksmn <al-mn@yandex.ru>
2023-08-29 16:53:25 +0300

    fix

--- Algorithms/check_time.py
+++ Algorithms/check_time.py
@@ -3,14 +3,6 @@ import timeit
 
 nums = [-22, -3, -1, -9, -12, -19, -10]
 
-
-def flawed(a: list):
-    largest = 0
-    for n in a:
-        if largest < n:
-            largest = n
-    return largest
-
 def get_largest(a: list):
     largest = a[0]
     for i in range(1, len(a)):
@@ -28,7 +20,6 @@ def get_largest_2(a: list):
 
 reps = 1000000
 
-print('flawed:', timeit.timeit(lambda: flawed(nums), number=reps))
 print('get_largest:', timeit.timeit(lambda: get_largest(nums), number=reps))
 print('get_largest_2:', timeit.timeit(lambda: get_largest_2(nums), number=reps))
 
