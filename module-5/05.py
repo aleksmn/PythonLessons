4cd966d811e33bc54b0788e2d3c7c52cd188bb24
aleksmn <al-mn@yandex.ru>
2023-09-13 17:30:45 +0300

    fix

--- module-5/05.py
+++ module-5/05.py
@@ -1,22 +1,52 @@
+# Пример инкапсуляции
+
+# class Kettle:
+#     def turn_on(self):
+#         print('Чайник включился')
+#         self.boil()
+#         self.check_t()
+#         self.beep()
+#         self.turn_off()
+
+#     def boil(self):
+#         print('Вода греется...')
+
+#     def check_t(self):
+#         print('Проверяется температура')
+
+#     def beep(self):
+#         print('Вода нагрелась, дается сигнал')
+
+#     def turn_off(self):
+#         print('Чайник выключился')
+
+
+# chaynik = Kettle()
+# chaynik.turn_on()
+
+
+
+# Пример инкапсуляции
 
 class Kettle:
     def turn_on(self):
         print('Чайник включился')
-        self.boil()
-        self.check_t()
-        self.beep()
-        self.turn_off()
+        self.__boil()
+        self.__check_t()
+        self.__beep()
+        self._turn_off()
 
-    def boil(self):
+    # Private
+    def __boil(self):
         print('Вода греется...')
 
-    def check_t(self):
+    def __check_t(self):
         print('Проверяется температура')
 
-    def beep(self):
+    def __beep(self):
         print('Вода нагрелась, дается сигнал')
-
-    def turn_off(self):
+    # Protected
+    def _turn_off(self):
         print('Чайник выключился')
 
 
