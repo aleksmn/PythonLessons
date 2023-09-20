3cda5f9c3b489dec21f1b81f1d5c04eaf854582d
aleksmn <al-mn@yandex.ru>
2023-09-20 17:03:12 +0300

    fix

--- module-5/speech/02.py
+++ module-5/speech/02.py
@@ -1,17 +1,28 @@
 import speech_recognition as sr
 
-recognizer = sr.Recognizer()
+def get_speech():
+    text = ''
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Слушаю...")
+        audio = recognizer.listen(source)
+        print("Конец связи")
+
+    try:
+        text = recognizer.recognize_google(audio, language='ru-RU')
+        print(f'Вы сказали: {text}')
+    except sr.UnknownValueError:
+        print("Не понял вас.")
+    except sr.RequestError as e:
+        print(f"Ошибка сервиса распознавания речи: {e}.")
+
+    finally:
+        return text
 
 
-with sr.Microphone() as source:
-    print("Слушаю...")
-    audio = recognizer.listen(source)
-    print("Конец связи")
+if __name__ == "__main__":
+    speech = get_speech()
 
-try:
-    text = recognizer.recognize_google(audio, language='ru-RU')
-    print(f'Вы сказали: {text}')
-except sr.UnknownValueError:
-    print("Не понял вас.")
-except sr.RequestError as e:
-    print(f"Ошибка сервиса распознавания речи: {e}.")
+    if 'привет' in speech.lower():
+        print("И тебе привет!")
