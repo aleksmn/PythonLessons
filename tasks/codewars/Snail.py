f0da4239508543947f8a51f41f0ca591aabcf0e3
aleksmn <al-mn@yandex.ru>
2023-10-16 11:01:34 +0300

    init

--- tasks/codewars/Snail.py
+++ tasks/codewars/Snail.py
@@ -1,56 +1,32 @@
-def spiralPrint(a):
 
-    m = n = len(a)
+def snail(m):
+    n = len(m)
+    result = []
+    start_x, start_y = 0, 0
+    end_x, end_y = n - 1, n - 1
 
-    k = 0
-    l = 0
- 
-    ''' k - starting row index
-        m - ending row index
-        l - starting column index
-        n - ending column index
-        i - iterator '''
- 
-    while (k < m and l < n):
- 
-        # Print the first row from
-        # the remaining rows
-        for i in range(l, n):
-            print(a[k][i], end=" ")
- 
-        k += 1
- 
-        # Print the last column from
-        # the remaining columns
-        for i in range(k, m):
-            print(a[i][n - 1], end=" ")
- 
-        n -= 1
- 
-        # Print the last row from
-        # the remaining rows
-        if (k < m):
- 
-            for i in range(n - 1, (l - 1), -1):
-                print(a[m - 1][i], end=" ")
- 
-            m -= 1
- 
-        # Print the first column from
-        # the remaining columns
-        if (l < n):
-            for i in range(m - 1, k - 1, -1):
-                print(a[i][l], end=" ")
- 
-            l += 1
- 
- 
-# Driver Code
-a = [[1, 2, 3, 4],
-     [5, 6, 7, 8],
-     [9, 10, 11, 12],
-     [13, 14, 15, 16]]
- 
+    x = start_x
+    while x <= end_x:
+        result.append(m[start_y][x])
+        x += 1
 
-# Function Call
-spiralPrint(a)
+    y = start_y + 1
+    while y <= end_y:
+        result.append(m[y][end_x])
+        y += 1
+
+
+    return result
+
+
+
+
+array = [[1,2,3],
+         [8,9,4],
+         [7,6,5]]
+
+print(snail(array))
+
+
+
+
