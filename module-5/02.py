7fc7a1184aa228066d5d4a5e26c065d3bda0e50a
aleksmn <al-mn@yandex.ru>
2023-11-12 14:19:45 +0300

    fix

--- module-5/02.py
+++ module-5/02.py
@@ -105,12 +105,23 @@ print(plane3.engines)
 
 
 
-# class Cat:
-#     def __init__(self, name, breed, coat_color, character):
-#       self.name = name
-#       self.breed = breed
-#       self.coat_color = coat_color
-#       self.character = character
-#       print('Котик родился!')
-#       print(f'Теперь его зовут {self.name}!')
+class Cat:
+    def __init__(self, name, breed, coat_color, character):
+        self.name = name
+        self.breed = breed
+        self.coat_color = coat_color
+        self.character = character
+        print('Котик родился!')
+        print(f'Теперь его зовут {self.name}!')
 
+    def speak(self, text: str):
+        """Метод класса"""
+        print(f"Кот по имени {self.name} говорит Мяу! {text.capitalize()}.")
+
+
+
+murzik = Cat("Мурзик", "персидский", "рыжий", "веселый")
+    
+
+
+    
