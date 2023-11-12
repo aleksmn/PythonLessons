d81f547478e2ba443f11e6ae14a066c2b99eb373
aleksmn <al-mn@yandex.ru>
2023-11-12 14:20:48 +0300

    fix

--- module-5/03.py
+++ module-5/03.py
@@ -1,19 +1,17 @@
 
 class Cat:
-    def __init__(self, name, color, character):
-      self.name = name
-      self.color = color
-      self.character = character
-      print('Котик родился!')
-      print(f'Теперь его зовут {self.name}!')
+    def __init__(self, name, breed, coat_color, character):
+        self.name = name
+        self.breed = breed
+        self.coat_color = coat_color
+        self.character = character
+        print('Котик родился!')
+        print(f'Теперь его зовут {self.name}!')
+
+    def speak(self, text: str):
+        """Метод класса"""
+        print(f"Кот по имени {self.name} говорит Мяу! {text.capitalize()}.")
 
 
-    def say(self):
-       print(f'{self.character.capitalize()} {self.color} котенок {self.name} говорит Мяу!')
 
-
-
-kitty = Cat('Вася', 'рыжий', 'веселый')
-
-
-kitty.say()
+murzik = Cat("Мурзик", "персидский", "рыжий", "веселый")
