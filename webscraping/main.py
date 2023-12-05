62b90abeafacea74b7d4b76bcc670af6bc5450d0
aleksmn <al-mn@yandex.ru>
2023-12-05 16:20:12 +0300

    fix

--- webscraping/main.py
+++ webscraping/main.py
@@ -14,26 +14,26 @@ doc = BeautifulSoup(source.text, 'html.parser')
 
 olimpiads = doc.find_all('div', class_="olympiads__content")
 
-# print(olimpiads[1].h2.text.strip())
+print(olimpiads[1].h2.text.strip())
 
-# for o in olimpiads:
-#     title = o.h2.text.strip()
-#     base_link = "https://www.ucheba.ru/"
-#     link = base_link + o.h2.a['href']
-#     classes = o.find(class_='class__value').text.strip()
-#     print(title, end=" ")
-#     print(link, end=" ")
-#     print("Классы:", classes)
+for o in olimpiads:
+    title = o.h2.text.strip()
+    base_link = "https://www.ucheba.ru/"
+    link = base_link + o.h2.a['href']
+    classes = o.find(class_='class__value').text.strip()
+    print(title, end=" ")
+    print(link, end=" ")
+    print("Классы:", classes)
 
 
-# with open('olimpiads.txt', 'w', encoding="utf-8") as f:
-#     for o in olimpiads:
-#         title = o.h2.text.strip()
-#         base_link = "https://www.ucheba.ru/"
-#         link = base_link + o.h2.a['href']
-#         classes = o.find(class_='class__value').text.strip()
-#         line = f'{title}; {classes}; {link}\n'
-#         f.write(line)
+with open('olimpiads.txt', 'w', encoding="utf-8") as f:
+    for o in olimpiads:
+        title = o.h2.text.strip()
+        base_link = "https://www.ucheba.ru/"
+        link = base_link + o.h2.a['href']
+        classes = o.find(class_='class__value').text.strip()
+        line = f'{title}; {classes}; {link}\n'
+        f.write(line)
 
 
 
