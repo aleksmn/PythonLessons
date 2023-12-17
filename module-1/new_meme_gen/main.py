4eb94bf767b06b1098d65c56a19417b0dc200451
aleksmn <al-mn@yandex.ru>
2023-12-17 18:00:10 +0300

    fix

--- module-1/new_meme_gen/main.py
+++ module-1/new_meme_gen/main.py
@@ -19,10 +19,12 @@ else:
 
 print(top_text, bottom_text)
 
+
 # Шаг 2
 memes = ["Кот в ресторане.png", "Кот в очках.png"]
 
 print("Выберите картинку для мема:")
+
 for i in range(len(memes)):
     print(i, memes[i])
 
@@ -39,13 +41,23 @@ draw = ImageDraw.Draw(image)
 
 font = ImageFont.truetype('arial.ttf', size=70)
 
-text = draw.textbbox((0, 0), top_text, font)
-text_width, text_height = text[2], text[3]
-draw.text(((width - text_width) / 2, 30), top_text, font=font, fill="black")
 
-text = draw.textbbox((0, 0), bottom_text, font)
-text_width, text_height = text[2], text[3]
-draw.text(((width - text_width) / 2, height - text_height - 30), bottom_text, font=font, fill="black")
+draw.text((100, 30), top_text, font=font, fill="black")
+draw.text((100, height - 200), bottom_text, font=font, fill="black")
 
 
 image.save("new_meme.jpg")
+
+
+
+
+
+
+
+# Текст по центру:
+# text = draw.textbbox((0, 0), top_text, font)
+# draw.text(((width - text[2]) / 2, 30), top_text, font=font, fill="black")
+
+# text = draw.textbbox((0, 0), bottom_text, font)
+# draw.text(((width - text[2]) / 2, height - text[3] - 30), bottom_text, font=font, fill="black")
+
