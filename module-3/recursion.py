74c5f084d5a886abd62e25f2bcf90eb7d1f5833c
aleksmn <al-mn@yandex.ru>
2024-02-16 12:46:55 +0300

    fix

--- module-3/recursion.py
+++ module-3/recursion.py
@@ -1,20 +1,82 @@
-
+# Сумма всех чисел до n включительно 
 n = 7
 res = 0
 for i in range (1, n+1):
     res += i
 print(res)
 
+# 1 + 2 + 3 + 4 + 5 + 6 + 7
+
 
 
 # Рекурсия
+
 def sum_range(n):
 	if n == 1:
 		return 1
+    # рекурсия - вызов функцией самой себя
 	return n + sum_range(n - 1)
 
 
 print(sum_range(7))
+
+
+
+
+
+
+
+
+def recursion(n):
+    print(n)
+	
+    if n == 1:
+        return
+    
+    return recursion(n - 1)
+
+
+
+recursion(7)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -41,11 +103,11 @@ print(sum_range(7))
 
 
 
-def recursion(n):
-    print(n)
-    if n == 1: 
-      return 1
-    return recursion(n-1)
+# def recursion(n):
+#     print(n)
+#     if n == 1: 
+#       return 1
+#     return recursion(n-1)
 
 
-recursion(int(input("Введите число: ")))
+# recursion(int(input("Введите число: ")))
