f008c0a6d1345b26efcc543950e31daae78d32d2
aleksmn <al-mn@yandex.ru>
2024-02-17 17:39:17 +0300

    add example

--- module-5/03.py
+++ module-5/03.py
@@ -12,7 +12,40 @@ class Cat:
         print(f"Кот по имени {self.name} говорит: Мяу! {text.capitalize()}. Мяу-мяу!")
 
 
+    # def __str__(self):
+    #     return f"Объект класса Cat. Имя: {self.name}"
 
-murzik = Cat("Мурзик", "персидский", "рыжий", "веселый")
 
-murzik.speak("всем привет")
+
+# murzik = Cat("Мурзик", "персидский", "рыжий", "веселый")
+
+# murzik.speak("всем привет")
+
+# print(murzik)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    # метод класса
+    def get_perimeter(self):
+        return (self.width + self.height) * 2
+    
+
+
+rect_1 = Rectangle(23, 54)
+print(rect_1.get_perimeter())
