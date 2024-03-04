6e22ff9a0b6ecd3c2d554c4b288797e3628f20ce
aleksmn <al-mn@yandex.ru>
2024-03-04 12:23:16 +0300

    fix

--- module-4/finance/bank.py
+++ module-4/finance/bank.py
@@ -1,16 +1,15 @@
 def suggestions():
     print("Предложения SkysmartBank")
     with open("suggestions.txt", "r", encoding='utf-8') as file:
-        for line in file:
-            # .strip убирает все пробелы по краям и переходы на новую строку
-            print(line.strip())        
-
+        for line in file:  
+            print(line.strip())
 
 
 def complain():
     text = input("Введите текст жалобы: ")
+    # "a" - дописать
     with open("complains.txt", 'a', encoding="utf-8") as file:
-        file.write("\n" + text)
+        file.write(text + "\n")
 
     print("Ваша жалоба будет рассмотрена в скором времени.")
 
