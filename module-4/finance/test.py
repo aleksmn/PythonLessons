6c1d3abe737fd63d63cfdcba34af3607ca8a2af8
aleksmn <al-mn@yandex.ru>
2024-03-18 11:36:37 +0300

    fix

--- module-4/finance/test.py
+++ module-4/finance/test.py
@@ -1,48 +1,62 @@
 import json
 
-
+# Открываем файл JSON для чтения данных
+# read - r - для чтения
 with open("client_info.json", "r", encoding="utf-8") as json_file:
+    # загружаем данныхе из файла
     data = json.load(json_file)
 
-# print(data)
+
+# Выведем только первый счет пользователя
+print(data["accounts"][0])
 
 
-# print(type(data))
+# Выведем транзакции
+# только последнюю транзакцию
+print(data["transactions"][-1]["amount"])
+
 
 # Изменим имя
-data["name"] = "Людмила Иванова"
+data["name"] = "Иван Иванович"
 
 
-# Добавим счет
 
-data["accounts"].append({
-            "name": "На путешествие",
-            "system": "MasterCard Mass",
-            "number": "5555 0000 5555 0000",
-            "type": "дебетовая",
-            "balance": 25000,
-            "validity period": "09/2024"
-})
+#  Добавим счет
+
+data["accounts"].append(
+    {
+        "name": "На отпуск",
+        "system": "MasterCard Mass",
+        "number": "1111 0000 2222 0000",
+        "type": "дебетовая",
+        "balance": 456778,
+        "validity period": "09/2024"
+    }
+)
+
+
 
 print(data)
 
 
+#  Сохраняем изменения обратно в файл
 
-
-
-# Сохраняем обратно в файл
-
+# w - открываем файл для записи
 with open("client_info.json", "w", encoding="utf-8") as json_file:
     json.dump(data, json_file, ensure_ascii=False)
 
 
 
-    
 
-# Выводим транзакции
-for transaction in data['transactions']:
-    print("Аккаунт: ", transaction['account'])
-    print("Тип: ", transaction['type'])
-    print("Дата: ", transaction['date']['year'], transaction['date']['month'])
-    print("Сумма: ", transaction['amount'])
-    print('--------------------------------------')
+
+
+
+
+
+# # Выводим транзакции
+# for transaction in data['transactions']:
+#     print("Аккаунт: ", transaction['account'])
+#     print("Тип: ", transaction['type'])
+#     print("Дата: ", transaction['date']['year'], transaction['date']['month'])
+#     print("Сумма: ", transaction['amount'])
+#     print('--------------------------------------')
