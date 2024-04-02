1570ee1c6af51160f97d65b3e0bf97ffeca3733f
aleksmn <al-mn@yandex.ru>
2024-04-02 19:30:20 +0300

    fix

--- module-3/01.py
+++ module-3/01.py
@@ -1,61 +1,31 @@
-
-
-username = 'User'
+# Локальные и глобальные переменные
 
 # Глобальная переменная
-res = 0
+username = "User"
+color = "purple"
 
-def summa(a, b):
-    # Глобальная переменная доступна для чтения внутри функции
-    print(username)
 
-    #username = 'Василий'  # ошибка, для изменения переменная не доступна
+def get_user():
+
+    # глобальная переменная внутри функции
+    global color
+    color = "red"
 
     # Локальная переменная
-    example = 6
-
-    # Изменяем глобальную переменную внутри функции
-    global res
-    res = a + b
-
-    print(locals())
-    print(globals())
-    # Глобальное пространство имен
-    print(dir(__builtins__))
-    
+    username = "Дмитрий"
 
 
-summa(5, 8)
+    print("Имя пользователя", username)
+    print("Любимый цвет", color)
 
-print(res)
+    print("Локальное пространство имен",locals())
+    print("Глобальное пространство имен", globals())
 
 
 
 
+# Вызываем функцию    
+get_user()
 
-
-
-# Создай функцию multiply_range(start, end), которая перемножает все целые числа от значения "start" до значения "end" включительно.
-
-# Если значение start больше, чем end, то они меняются местами.
-
-
-
-def multiply_range(start:int, end:int) -> int:
-    '''Перемножаем числа от start до end'''
-
-    if start > end:
-        start, end = end, start
-
-    res = 1
-    # Перемножение
-    for i in range(start, end+1):
-        res *= i
-
-    return res
-
-
-
-# Вызов функции
-print(multiply_range(10, 5))
-
+print(username)
+print(color)
