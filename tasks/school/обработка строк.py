b7c465b482d73aba55fbdeb1276d6ceb3b8ece55
aleksmn <al-mn@yandex.ru>
2024-04-05 16:53:12 +0300

    fix

--- tasks/school/обработка строк.py
+++ tasks/school/обработка строк.py
@@ -17,11 +17,9 @@
 '''
 
 
-# 1) открываем файл
-# 2) перебираем с помощью цикла for
-# 3) подсчитываем последовательности по условию
 
 
+#  1
 f = open("string_1.txt").readline()
 
 # XXYXXXZXXZXYXXXXYYYXXXXYYYXXXYYXYZYXYYXYXXX
@@ -42,7 +40,39 @@ for i in range(1, len(f)):
 print(max_counter)
 
 
+#  2
+f = open("string_1.txt").readline()
 
+
+counter = 1
+max_counter = 0
+
+for i in range(1, len(f)):
+    # print(f[i], f[i-1])
+    if f[i] == f[i-1] == "X":
+        counter += 1
+    else:
+        if counter > max_counter:
+             max_counter = counter
+        
+        counter = 1
+
+print(max_counter)
+
+
+
+# 3
+f = open("string_1.txt").readline()
+k = mx = 0
+for i in range(len(f)):
+    if f[i-1:i+1] in 'XYZX' and k:
+        k += 1
+    elif f[i] == 'X':
+        k = 1
+    else:
+        k = 0
+    mx = max(mx, k)
+print(mx)
 
 
 
