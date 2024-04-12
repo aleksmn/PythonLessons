da3d1c827ede0b7e9cc8144313cc45e03513f28b
aleksmn <al-mn@yandex.ru>
2024-04-12 12:10:56 +0300

    add methods

--- module-5/rectangle.py
+++ module-5/rectangle.py
@@ -7,13 +7,22 @@ class Rectangle:
         self.height = height
         print(f"Прямоугольник со сторонами {self.width} и {self.height} создан")
 
+    # метод для нахождения периметра
+    def get_perimeter(self):
+        return self.width * 2 + self.height * 2
+
+    # метод для нахождения площади
+    def get_area(self):
+        return self.width * self.height
+
+
 
 
 rect1 = Rectangle(2, 4)
 rect2 = Rectangle(34, 45)
 rect3 = Rectangle(23, 56)
 
-# Выводим периметры
-print(rect1.width * 2 + rect1.height * 2)
-print(rect2.width * 2 + rect2.height * 2)
-print(rect3.width * 2 + rect3.height * 2)
+
+print(rect1.get_perimeter())
+print(rect2.get_perimeter())
+print(rect3.get_perimeter())
