77c83afd67b0e7ffd9c2aebea5efd71ed8fdfbb9
aleksmn <al-mn@yandex.ru>
2024-07-28 19:52:58 +0300

    fix

--- module-5/battleships/main copy 2.py
+++ module-5/battleships/main copy 2.py
@@ -7,9 +7,23 @@ class Field:
         self.ships_alive = ships
         self.grid = []
 
-        # создаем пустую сетку
-        for _ in range(size):
-            self.grid.append([None] * size)
+        for i in range(size):
+            self.grid.append([None] * 10)
+
+
+    def display(self, show_ships=False):
+        letter_string = "    A B C D E F G H I J"
+        print(letter_string)
+        # Номер строки
+        counter = 1
+
+        for row in self.grid:
+            display_row = ""
+            for cell in row:
+                display_row += "O "
+
+            print(f"{counter:2d}  {display_row}")
+            counter += 1
 
 
 
@@ -19,4 +33,4 @@ class Field:
 if __name__ == "__main__":
     field = Field(10, 15)
 
-    print(field.grid)
+    field.display(show_ships=True)
