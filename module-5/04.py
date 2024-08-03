943cbc0a3419fc5b153232105a990485fd59df67
aleksmn <al-mn@yandex.ru>
2024-08-03 16:05:27 +0300

    fix

--- module-5/04.py
+++ module-5/04.py
@@ -1,113 +1,113 @@
-# class Cat():
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.age = age
+class Cat():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
-#     def __str__(self):
-#         return f"Кошка по имени {self.name}. Возраст: {self.age}."
+    def __str__(self):
+        return f"Кошка по имени {self.name}. Возраст: {self.age}."
 
-#     def speak(self):
-#         print("Мяу-мяу!")
-
-
-
-# class Dog():
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.age = age
-
-#     def __str__(self):
-#         return f"Собака по имени {self.name}. Возраст: {self.age}."
-
-#     # Полиморфизм: название метода одинаковое - разные действия
-#     def speak(self):
-#         print("Гав-гав!")
+    def speak(self):
+        print("Мяу-мяу!")
 
 
 
+class Dog():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
 
+    def __str__(self):
+        return f"Собака по имени {self.name}. Возраст: {self.age}."
 
-# my_cat = Cat("Мурка", 2.5)
-
-# print(my_cat.sleep())
-
-
-
-# my_dog = Dog("Барбос", 4)
-
-# print(my_cat)
-# my_cat.speak()
-
-
-# print(my_dog)
-# my_dog.speak()
+    # Полиморфизм: название метода одинаковое - разные действия
+    def speak(self):
+        print("Гав-гав!")
 
 
 
-# class Animal:
-#     def __init__(self, name, age):
-#         self.name = name
-#         self.age = age
 
-#     def run(self):
-#         print("Бегаю!!!")
 
-#     def sleep(self):
-#         print("Сплю...")
+my_cat = Cat("Мурка", 2.5)
+
+print(my_cat.sleep())
 
 
 
-# class Cat(Animal):
+my_dog = Dog("Барбос", 4)
 
-#     def __init__(self, name, age):
-#         super().__init__(name, age)
-
-
-#     def __str__(self):
-#         return f"Кошка по имени {self.name}. Возраст: {self.age}."
-
-#     def speak(self):
-#         print("Мяу-мяу!")
-
-#     def scratch(self):
-#         print("Точу когти!")
+print(my_cat)
+my_cat.speak()
 
 
-# class Dog(Animal):
-#     def __init__(self, name, age):
-#         super().__init__(name, age)
-
-#     def __str__(self):
-#         return f"Собака по имени {self.name}. Возраст: {self.age}."
-
-#     # Полиморфизм: один метод - разные действия
-#     def speak(self):
-#         print("Гав-гав!")
+print(my_dog)
+my_dog.speak()
 
 
 
-# class Tiger(Cat):
-#     def __init__(self, name, age):
-#         super().__init__(name, age)
+class Animal:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def run(self):
+        print("Бегаю!!!")
+
+    def sleep(self):
+        print("Сплю...")
 
 
-# my_cat = Cat("Мурка", 2.5)
-# my_dog = Dog("Барбос", 4)
 
-# print(my_cat)
-# my_cat.speak()
-# my_cat.sleep()
+class Cat(Animal):
 
-# print(my_dog)
-# my_dog.speak()
-# my_dog.sleep()
+    def __init__(self, name, age):
+        super().__init__(name, age)
 
 
-# tiger_1 = Tiger("Тигра", 8)
-# print(tiger_1)
-# tiger_1.scratch()
+    def __str__(self):
+        return f"Кошка по имени {self.name}. Возраст: {self.age}."
+
+    def speak(self):
+        print("Мяу-мяу!")
+
+    def scratch(self):
+        print("Точу когти!")
 
 
-# for animal in (my_cat, my_dog):
-#     print(animal)
-#     animal.speak()
+class Dog(Animal):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+    def __str__(self):
+        return f"Собака по имени {self.name}. Возраст: {self.age}."
+
+    # Полиморфизм: один метод - разные действия
+    def speak(self):
+        print("Гав-гав!")
+
+
+
+class Tiger(Cat):
+    def __init__(self, name, age):
+        super().__init__(name, age)
+
+
+my_cat = Cat("Мурка", 2.5)
+my_dog = Dog("Барбос", 4)
+
+print(my_cat)
+my_cat.speak()
+my_cat.sleep()
+
+print(my_dog)
+my_dog.speak()
+my_dog.sleep()
+
+
+tiger_1 = Tiger("Тигра", 8)
+print(tiger_1)
+tiger_1.scratch()
+
+
+for animal in (my_cat, my_dog):
+    print(animal)
+    animal.speak()
