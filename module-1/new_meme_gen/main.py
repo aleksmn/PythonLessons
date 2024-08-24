be3096a8c1d0160371c46444dbf8b907e727eb17
aleksmn <al-mn@yandex.ru>
2024-08-24 19:47:06 +0300

    fix

--- module-1/new_meme_gen/main.py
+++ module-1/new_meme_gen/main.py
@@ -1,7 +1,9 @@
 from PIL import Image, ImageDraw, ImageFont
 
-# установим библиотеку Pillow
+# Как установить библиотеку (модуль)
+# выполнить в терминале команду
 # pip install pillow
+
 
 print("Генератор мемов запущен!")
 
@@ -9,59 +11,53 @@ text_type = input("Сколько добавить текстов, 1 или 2: "
 
 if text_type == "1":
     top_text = ""
-    bottom_text = input("Введите нижний текст: ")
+    bottom_text = input("Введите текст: ")
+
 elif text_type == "2":
     top_text = input("Введите верхний текст: ")
     bottom_text = input("Введите нижний текст: ")
+
 else:
     print("Введён неправильный режим. Перезапустите программу.")
     # Выход из программы
     quit()
 
-print(top_text, bottom_text)
-
-# Шаг 2
-# Список картинок
-memes = ["Кот в ресторане.png", "Кот в очках.png"]
-
 print("Выберите картинку для мема:")
 
-count = 1
-for m in memes:
-    print(count, m)
-    count += 1
+print("1. Кот в ресторане")
+print("2. Кот в очках")
 
 
-meme_number = int(input("Введите номер картинки: "))
+meme_number = input("Введите номер картинки: ")
 
-# Шаг 3. Открываем картинку
+if meme_number == "1":
+    meme = "Кот в ресторане.png"
+elif meme_number == "2":
+    meme = "Кот в очках.png"
+else:
+    print("Введён неправильный номер картинки. Перезапустите программу.")
+    # Выход из программы
+    quit()
 
-image = Image.open(memes[meme_number - 1])
+
+#  Открываем картинку
+image = Image.open(meme)
+
 width, height = image.size
 
 print(width, height)
 
 draw = ImageDraw.Draw(image)
 
-#for macOS:  "Keyboard.ttf"
-
 font = ImageFont.truetype('arial.ttf', size=70)
 
-# Добавим текст на картинку
-# верхний текст
-draw.text(xy=(width / 2, 60), 
-          text=top_text, 
-          font=font, 
-          fill="black", 
-          anchor="mt")
 
-# нижний текст
-draw.text(xy=(width / 2, height - 60), 
-          text=bottom_text, 
-          font=font, 
-          fill="black", 
-          anchor="mb")
+# Добавим верхний текст на картинку
 
+draw.text(xy=(width // 2, 60), text=top_text, font=font, fill="black", anchor="mt")
 
+draw.text(xy=(width // 2, height - 60), text=bottom_text, font=font, fill="black", anchor="mb")
+
+# Сохраняем картинку
 image.save("new_meme.png")
 print("Картинка сохранена!")
