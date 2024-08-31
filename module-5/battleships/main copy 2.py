2e553f0f63c7f413e791098db7653baa3d4e7856
aleksmn <al-mn@yandex.ru>
2024-08-31 16:36:16 +0300

    init

--- module-5/battleships/main copy 2.py
+++ module-5/battleships/main copy 2.py
@@ -4,33 +4,7 @@ import random
 class Field:
     def __init__(self, size, ships):
         self.size = size
-        self.ships_alive = ships
-        self.grid = []
-
-        for i in range(size):
-            self.grid.append([None] * 10)
+        self.ships_alive = ships   # количество кораблей
+        self.grid = []   # поле для игры
 
 
-    def display(self, show_ships=False):
-        letter_string = "    A B C D E F G H I J"
-        print(letter_string)
-        # Номер строки
-        counter = 1
-
-        for row in self.grid:
-            display_row = ""
-            for cell in row:
-                display_row += "O "
-
-            print(f"{counter:2d}  {display_row}")
-            counter += 1
-
-
-
-
-
-# Запуск программы
-if __name__ == "__main__":
-    field = Field(10, 15)
-
-    field.display(show_ships=True)
