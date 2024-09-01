ea4865ff5d13040c4245a002472e36fe91d60489
aleksmn <al-mn@yandex.ru>
2024-09-01 18:20:11 +0300

    add examples

--- module-2/list_demo.py
+++ module-2/list_demo.py
@@ -1,3 +1,25 @@
 # Список (list)
 
-nums = [12, 34, 67, 1, 34, 2]
+
+#       0    1  2           -1
+nums = [12, 43, 1, 3, 5, 3, 1]
+
+print(type(nums))
+
+# Индексы
+print(nums[0])
+print(nums[-1])
+
+# замена по индексу
+nums[3] = 99
+print(nums)
+
+# Срезы
+print(nums[:3])
+print(nums[3:])
+
+
+
+
+
+
