0ce0caf0757cb64c873d0c9d9859a16b56d28abf
aleksmn <al-mn@yandex.ru>
2024-10-01 17:05:09 +0300

    init

---
+++ module-1/turtle-demo/goto.py
@@ -0,0 +1,26 @@
+import turtle
+
+# Настройки
+turtle.shape("turtle")
+turtle.color("aqua")
+turtle.bgcolor("black")
+
+
+
+# Движение
+
+# Перемещение по координатам
+#            x    y
+turtle.goto(200, -200)
+turtle.goto(200, 200)
+turtle.goto(0, 0)
+
+
+
+
+
+
+
+# Запуск основного цикла
+# всегда последняя строка
+turtle.mainloop()
