6d78d29f1b6f1815fb321d22651baf94fa86be5c
aleksmn <al-mn@yandex.ru>
2025-05-19 19:07:55 +0300

    fix

--- module-5/05.py
+++ module-5/05.py
@@ -2,31 +2,64 @@
 
 class Kettle:
     def __init__(self):
-         self.__brand = "Скайсмарт"
+         self.brand = "Скайсмарт"
 
     def turn_on(self):
-        print(f'Чайник {self.__brand} включился')
-        self.__boil()
-        self.__check_t()
-        self.__beep()
-        self.__turn_off()
+        print(f'Чайник {self.brand} включился')
+        self.boil()
+        self.check_t()
+        self.beep()
+        self.turn_off()
 
-    # Private - скрытый метод
-    def __boil(self):
+    def boil(self):
         print('Вода греется...')
 
-    def __check_t(self):
+    def check_t(self):
         print('Проверяется температура')
 
-    def __beep(self):
+    def beep(self):
         print('Вода нагрелась, дается сигнал')
 
-    def __turn_off(self):
+    def turn_off(self):
         print('Чайник выключился')
 
 
 chaynik = Kettle()
 chaynik.turn_on()
+
+
+
+
+
+
+
+# class Kettle:
+#     def __init__(self):
+#          self.__brand = "Скайсмарт"
+
+#     def turn_on(self):
+#         print(f'Чайник {self.__brand} включился')
+#         self.__boil()
+#         self.__check_t()
+#         self.__beep()
+#         self.__turn_off()
+
+#     # Private - скрытый метод
+#     def __boil(self):
+#         print('Вода греется...')
+
+#     def __check_t(self):
+#         print('Проверяется температура')
+
+#     def __beep(self):
+#         print('Вода нагрелась, дается сигнал')
+
+#     def __turn_off(self):
+#         print('Чайник выключился')
+
+
+# chaynik = Kettle()
+# chaynik.turn_on()
 
 
 
